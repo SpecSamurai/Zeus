324dabb57abec8296a9889032216e8a71c1338fc
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-18 17:25:55 +0200

    feature(Editor): Add TemplateConfig class

---
+++ src/Editor/Models/TemplateConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Editor.Models;
+
+public class TemplateConfig
+{
+    public required string Name { get; init; }
+    public required string ProjectFile { get; init; }
+    public required string IconFile { get; init; }
+    public required string ScreenshotFile { get; init; }
+    public required string[] Folders { get; init; }
+}
