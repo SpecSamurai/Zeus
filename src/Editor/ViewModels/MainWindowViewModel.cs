eb08c9ff69096d1c0f76d3621240a644403db546
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-19 22:33:52 +0200

    feature(Editor): Configure MainWindow to handle Project Browser dialog

--- src/Editor/ViewModels/MainWindowViewModel.cs
+++ src/Editor/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Editor.ViewModels;
+﻿using Editor.ViewModels.ProjectBrowser;
+using ReactiveUI;
+using System.Reactive.Linq;
+using System.Windows.Input;
+
+namespace Editor.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public MainWindowViewModel()
+    {
+        ShowProjectBrowserDialog = new Interaction<ProjectBrowserWindowViewModel, ProjectViewModel?>();
+
+        OpenDialogCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            var model = new ProjectBrowserWindowViewModel();
+            Project = await ShowProjectBrowserDialog.Handle(model);
+        });
+    }
+
+    public Interaction<ProjectBrowserWindowViewModel, ProjectViewModel?> ShowProjectBrowserDialog { get; }
+    public ICommand OpenDialogCommand { get; }
+    public ProjectViewModel? Project { get; set; }
 }
--- src/Editor/Views/MainWindow.axaml.cs
+++ src/Editor/Views/MainWindow.axaml.cs
@@ -1,11 +1,33 @@
-using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.ReactiveUI;
+using Editor.ViewModels;
+using Editor.ViewModels.ProjectBrowser;
+using Editor.Views.ProjectBrowser;
+using ReactiveUI;
+using System.Reactive;
 
 namespace Editor.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+        this.WhenActivated(action => action(ViewModel!.ShowProjectBrowserDialog.RegisterHandler(ShowProjectBrowserDialogAsync)));
+        Loaded += ShowProjectBrowserDialogOnLoaded;
+    }
+
+    private async Task ShowProjectBrowserDialogAsync(InteractionContext<ProjectBrowserWindowViewModel, ProjectViewModel?> interaction)
+    {
+        var dialog = new ProjectBrowserWindow();
+        dialog.DataContext = interaction.Input;
+
+        var result = await dialog.ShowDialog<ProjectViewModel?>(this);
+        interaction.SetOutput(result);
+    }
+
+    private void ShowProjectBrowserDialogOnLoaded(object? sender, RoutedEventArgs e)
+    {
+        ViewModel!.OpenDialogCommand.Execute(Unit.Default);
     }
 }
