13b29a1136c344ead613c2a984cf9f26a1fa95a3
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-20 17:13:34 +0200

    feature(Editor): Add CreateProjectView

---
+++ src/Editor/Views/ProjectBrowser/CreateProjectView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.ProjectBrowser;
+
+public partial class CreateProjectView : UserControl
+{
+    public CreateProjectView()
+    {
+        InitializeComponent();
+    }
+}
