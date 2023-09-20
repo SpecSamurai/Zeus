cdf11f6f378b10a7fa31a7300218633954559633
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-20 21:27:39 +0200

    feature(Editor): Add OpenProjectView

---
+++ src/Editor/Views/ProjectBrowser/OpenProjectView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.ProjectBrowser;
+
+public partial class OpenProjectView : UserControl
+{
+    public OpenProjectView()
+    {
+        InitializeComponent();
+    }
+}
