8a9c0e9a8fae5275998a0e6fa69001142102b1f7
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-20 07:34:18 +0200

    feature(Editor): Add ProjectBrowserWindow

---
+++ src/Editor/Views/ProjectBrowser/ProjectBrowserWindow.axaml.cs
@@ -0,0 +1,16 @@
+using Avalonia.ReactiveUI;
+using Editor.ViewModels.ProjectBrowser;
+using ReactiveUI;
+
+namespace Editor.Views.ProjectBrowser;
+
+public partial class ProjectBrowserWindow : ReactiveWindow<ProjectBrowserWindowViewModel>
+{
+    public ProjectBrowserWindow()
+    {
+        InitializeComponent();
+        this.WhenActivated(action => action(ViewModel!.CreateProjectCommand.Subscribe(Close)));
+        this.WhenActivated(action => action(ViewModel!.OpenProjectCommand.Subscribe(Close)));
+        this.WhenActivated(action => action(ViewModel!.ExitProjectCommand.Subscribe(_ => Close())));
+    }
+}
