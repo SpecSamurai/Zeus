1e884626c0b6c939ea68daf2514e461c5c6215af
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-21 19:18:04 +0200

    feature(Editor): Remove ProjectFile property from TemplateFile

--- src/Editor/Models/Projects/TemplateFile.cs
+++ src/Editor/Models/Projects/TemplateFile.cs
@@ -3,6 +3,5 @@
 public class TemplateFile
 {
     public required string Name { get; init; }
-    public required string ProjectFile { get; init; }
     public required string[] Folders { get; init; }
 }
