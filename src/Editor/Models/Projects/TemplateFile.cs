53a970d4e1dfa631ce481b923958da1dff58bfd2
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-21 08:38:19 +0200

    feature(Editor): Add TemplateFile

---
+++ src/Editor/Models/Projects/TemplateFile.cs
@@ -0,0 +1,8 @@
+﻿namespace Editor.Models.Projects;
+
+public class TemplateFile
+{
+    public required string Name { get; init; }
+    public required string ProjectFile { get; init; }
+    public required string[] Folders { get; init; }
+}
