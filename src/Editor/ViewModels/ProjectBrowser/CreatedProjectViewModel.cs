615193db30d574f820e55071d7edc120930f4db0
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 21:30:43 +0200

    feature(Editor): Add CreatedProjectViewModel

---
+++ src/Editor/ViewModels/ProjectBrowser/CreatedProjectViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Media.Imaging;
+using Editor.Models.Projects;
+
+namespace Editor.ViewModels.ProjectBrowser;
+
+public class CreatedProjectViewModel
+{
+    public CreatedProjectViewModel(CreatedProject data)
+    {
+        Name = data.Name;
+        ProjectFilePath = data.ProjectFilePath;
+        Icon = new Bitmap(data.IconFilePath);
+        Screenshot = new Bitmap(data.ScreenshotFilePath);
+    }
+
+    public string Name { get; }
+    public string ProjectFilePath { get; }
+    public Bitmap Icon { get; }
+    public Bitmap Screenshot { get; }
+
+    public static CreatedProjectViewModel Create(CreatedProject data) =>
+        new(data);
+}
