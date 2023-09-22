703ecc0e3a9a22f0bff9ac3e5e695d367bbac72e
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 19:43:27 +0200

    feature(Editor): Add ControlExtensions

---
+++ src/Editor/Extensions/ControlExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+using DryIoc;
+
+namespace Editor.Extensions;
+
+public static class ControlExtensions
+{
+    public static void SetDesignDataContext<T>(this Control control)
+    {
+        if (Design.IsDesignMode && Design.GetDataContext(control) == null)
+            Design.SetDataContext(control, ServiceLocator.Container.Resolve<T>()!);
+    }
+}
