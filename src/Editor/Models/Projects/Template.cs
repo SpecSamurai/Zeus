612f4dce8b6ca90d1b1c6654a2b82bff76a29d3f
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 21:27:09 +0200

    feature(Editor): Add Template model

---
+++ src/Editor/Models/Projects/Template.cs
@@ -0,0 +1,10 @@
+﻿namespace Editor.Models.Projects;
+
+public class Template
+{
+    public required string Name { get; init; }
+    public required string[] Folders { get; init; }
+    public required string IconFilePath { get; init; }
+    public required string ScreenshotFilePath { get; init; }
+    public required string ProjectFilePath { get; init; }
+}
