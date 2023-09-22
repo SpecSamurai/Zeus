8a0a1980535a2dbdfeb3f63507bde12830453e92
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 21:27:20 +0200

    feature(Editor): Add CreatedProject model

---
+++ src/Editor/Models/Projects/CreatedProject.cs
@@ -0,0 +1,9 @@
+﻿namespace Editor.Models.Projects;
+
+public class CreatedProject
+{
+    public required string Name { get; init; }
+    public required string IconFilePath { get; init; }
+    public required string ScreenshotFilePath { get; init; }
+    public required string ProjectFilePath { get; init; }
+}
