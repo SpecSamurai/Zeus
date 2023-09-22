3e8c25e8cfe92def97ea286424a4490e9fc0eaa8
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 19:41:28 +0200

    feature(Editor): Add ProjectFiles constants

---
+++ src/Editor/Constants/ProjectFiles.cs
@@ -0,0 +1,11 @@
+namespace Editor.Constants;
+
+public static class ProjectFiles
+{
+    public const string ProjectFileExtension = ".zeus";
+
+    public const string SystemFolderName = ".Zeus";
+    public const string ProjectIconFileName = "Icon.png";
+    public const string ProjectScreenshotFileName = "Screenshot.png";
+    public const string ProjectFileName = $"project{ProjectFileExtension}";
+}
