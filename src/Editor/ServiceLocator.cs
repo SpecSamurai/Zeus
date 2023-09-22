d4a0c986740c3f6a263f707d38cc0a6cc56c2187
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 19:44:11 +0200

    feature(Editor): Add ServiceLocator

---
+++ src/Editor/ServiceLocator.cs
@@ -0,0 +1,28 @@
+﻿using DryIoc;
+using Editor.Repositories;
+using Editor.Services;
+using Editor.ViewModels;
+using Editor.ViewModels.ProjectBrowser;
+
+namespace Editor;
+
+internal class ServiceLocator
+{
+    static ServiceLocator()
+    {
+        Container = new Container();
+        RegisterServices();
+    }
+
+    public static Container Container { get; }
+
+    private static void RegisterServices()
+    {
+        Container.Register<IProjectFilesRepository, ProjectFilesRepository>();
+
+        Container.Register<IProjectFilesService, ProjectFilesService>();
+
+        Container.Register<MainWindowViewModel>();
+        Container.Register<ProjectBrowserWindowViewModel>();
+    }
+}
