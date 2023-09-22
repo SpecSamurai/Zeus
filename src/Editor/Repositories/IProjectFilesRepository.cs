f74c67308f9a4e217edd48ca5db48aa1af12fb24
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 20:32:45 +0200

    feature(Editor): Add ProjectFilesRepository interface

---
+++ src/Editor/Repositories/IProjectFilesRepository.cs
@@ -0,0 +1,10 @@
+﻿using Editor.Models.Projects;
+
+namespace Editor.Repositories;
+
+public interface IProjectFilesRepository
+{
+    IReadOnlyCollection<Template> GetTemplates();
+    IEnumerable<CreatedProject> GetCreatedProjects();
+    void UpdateProjectsFile(string projectName, string projectPath);
+}
