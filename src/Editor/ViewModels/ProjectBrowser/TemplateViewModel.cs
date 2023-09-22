aab99ee893ad718e371c2e396dc7059fe3d50e28
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 21:29:42 +0200

    feature(Editor): Add TemplateViewModel

---
+++ src/Editor/ViewModels/ProjectBrowser/TemplateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Media.Imaging;
+using Editor.Models.Projects;
+
+namespace Editor.ViewModels.ProjectBrowser;
+
+public class TemplateViewModel
+{
+    public TemplateViewModel(Template template)
+    {
+        Template = template;
+        Name = template.Name;
+        Icon = new Bitmap(template.IconFilePath);
+        Screenshot = new Bitmap(template.ScreenshotFilePath);
+    }
+
+    public Template Template { get; }
+    public string Name { get; }
+    public Bitmap Icon { get; }
+    public Bitmap Screenshot { get; }
+
+    public static TemplateViewModel Create(Template template) =>
+        new(template);
+}
