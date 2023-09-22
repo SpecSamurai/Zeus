4cfa016055c87361c10faf389dd9c1cd53397379
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-22 20:34:05 +0200

    feature(Editor): Add ProjectFilesService interface

---
+++ src/Editor/Services/IProjectFilesService.cs
@@ -0,0 +1,10 @@
+﻿using Editor.Models.Projects;
+using Editor.Repositories.Models;
+
+namespace Editor.Services;
+
+public interface IProjectFilesService
+{
+    ProjectFile OpenProject(string projectFilePath);
+    ProjectFile CreateProject((string RootPath, string Name) projectFolder, Template template);
+}
