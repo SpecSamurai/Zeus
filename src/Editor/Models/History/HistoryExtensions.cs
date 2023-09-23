ca180df8f4fec806b956a70f4b9b62cbef10b8cf
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-23 18:33:26 +0200

    feature(Editor): Add HistoryExtensions

---
+++ src/Editor/Models/History/HistoryExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Editor.Models.History;
+
+public static class HistoryExtensions
+{
+    public static void Add(this History history, string name, Action undo, Action redo)
+    {
+        history.Add(new HistoryCommand(name, undo, redo));
+    }
+}
