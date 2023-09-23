4263081a3d95817623e66fe8cac310cb496d8d98
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-23 18:33:14 +0200

    feature(Editor): Add History implementation

---
+++ src/Editor/Models/History/History.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Editor.Models.History;
+
+public sealed class History
+{
+    public History()
+    {
+        UndoList = new ObservableCollection<IHistoryCommand>();
+        RedoList = new ObservableCollection<IHistoryCommand>();
+    }
+
+    public ObservableCollection<IHistoryCommand> UndoList { get; }
+    public ObservableCollection<IHistoryCommand> RedoList { get; }
+
+    public void Add(IHistoryCommand command)
+    {
+        UndoList.Insert(0, command);
+        RedoList.Clear();
+    }
+
+    public void Undo()
+    {
+        if (UndoList.Any())
+        {
+            var command = UndoList.First();
+            UndoList.RemoveAt(0);
+            command.Undo();
+            RedoList.Add(command);
+        }
+    }
+
+    public void Redo()
+    {
+        if (RedoList.Any())
+        {
+            var command = RedoList.Last();
+            RedoList.RemoveAt(RedoList.Count - 1);
+            command.Redo();
+            UndoList.Insert(0, command);
+        }
+    }
+
+    public void Reset()
+    {
+        UndoList.Clear();
+        RedoList.Clear();
+    }
+}
