e5d46897a94265d171d96a1c70e18a5a95dabb86
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-24 09:42:20 +0200

    feature(Editor): Add History view and view model

---
+++ src/Editor/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Editor.Models.History;
+using System.Collections.ObjectModel;
+
+namespace Editor.ViewModels;
+
+public class HistoryViewModel : ViewModelBase
+{
+    private readonly History _undoRedoHistory;
+
+    public HistoryViewModel()
+    {
+        _undoRedoHistory = new History();
+    }
+
+    public ObservableCollection<IHistoryCommand> UndoList => _undoRedoHistory.UndoList;
+    public ObservableCollection<IHistoryCommand> RedoList => _undoRedoHistory.RedoList;
+
+    public void AddUndo(IHistoryCommand command)
+    {
+        _undoRedoHistory.Add(command);
+    }
+
+    public void Undo()
+    {
+        _undoRedoHistory.Undo();
+    }
+
+    public void Redo()
+    {
+        _undoRedoHistory.Redo();
+    }
+}
