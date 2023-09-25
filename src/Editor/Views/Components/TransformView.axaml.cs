19ccac532aa6f1cd652623a5b445b0f3e4ebe974
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-25 18:48:49 +0200

    feature(Editor): Add Transform view

---
+++ src/Editor/Views/Components/TransformView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.Components;
+
+public partial class TransformView : UserControl
+{
+    public TransformView()
+    {
+        InitializeComponent();
+    }
+}
