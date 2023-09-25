56d2ec6e09f888c4550f4272a2072a7d030d59ab
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-25 19:09:00 +0200

    feature(Editor): Add Script view

---
+++ src/Editor/Views/Components/ScriptView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.Components;
+
+public partial class ScriptView : UserControl
+{
+    public ScriptView()
+    {
+        InitializeComponent();
+    }
+}
