460bba7943504e6a02c675b55a71c8cd8ea2a36f
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-26 08:42:02 +0200

    feature(Editor): Add base Component view model

---
+++ src/Editor/ViewModels/Components/ComponentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Editor.ViewModels.WorldEditor;
+
+namespace Editor.ViewModels.Components;
+
+public class ComponentViewModel : ViewModelBase
+{
+    public ComponentViewModel(GameEntityViewModel owner)
+    {
+        Owner = owner;
+    }
+
+    public GameEntityViewModel Owner { get; set; }
+}
