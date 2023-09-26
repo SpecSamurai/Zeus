d0b2e1078a928653098f1de485dc90dceb48b954
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-26 14:33:10 +0200

    feature(Editor): Add Toolbar view

---
+++ src/Editor/Views/WorldEditor/ToolbarView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.WorldEditor;
+
+public partial class ToolbarView : UserControl
+{
+    public ToolbarView()
+    {
+        InitializeComponent();
+    }
+}
