04ca95e0547507e1bcb6f4c671694bb2591343c0
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-26 15:28:24 +0200

    feature(Editor): Add History view

---
+++ src/Editor/Views/WorldEditor/HistoryView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.WorldEditor;
+
+public partial class HistoryView : UserControl
+{
+    public HistoryView()
+    {
+        InitializeComponent();
+    }
+}
