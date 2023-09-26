36ff66f10ec9d69da1c57b52701522d596b4c578
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-26 15:25:25 +0200

    feature(Editor): Add Properties view

---
+++ src/Editor/Views/WorldEditor/PropertiesView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.WorldEditor;
+
+public partial class PropertiesView : UserControl
+{
+    public PropertiesView()
+    {
+        InitializeComponent();
+    }
+}
