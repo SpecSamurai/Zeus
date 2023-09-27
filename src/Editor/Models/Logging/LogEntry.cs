fb8fda0fe061a009825e499f90a517c77f93a430
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-27 16:21:32 +0200

    feature(Editor): Add LogEntry class

---
+++ src/Editor/Models/Logging/LogEntry.cs
@@ -0,0 +1,19 @@
+﻿namespace Editor.Models.Logging;
+
+public class LogEntry
+{
+    public LogEntry(LogLevel logLevel, string message, string file, string caller, int line)
+    {
+        LogLevel = logLevel;
+        Message = message;
+        File = file;
+        Caller = caller;
+        Line = line;
+    }
+
+    public LogLevel LogLevel { get; }
+    public string Message { get; }
+    public string File { get; }
+    public string Caller { get; }
+    public int Line { get; }
+}
