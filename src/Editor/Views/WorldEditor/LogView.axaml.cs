efcb63140e2d605c2441c51002e7427304713cf4
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-27 16:29:02 +0200

    feature(Editor): Add Log view

---
+++ src/Editor/Views/WorldEditor/LogView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.WorldEditor;
+
+public partial class LogView : UserControl
+{
+    public LogView()
+    {
+        InitializeComponent();
+    }
+}
