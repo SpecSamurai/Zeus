34136bb066ddc10d7264e3050a3fe41f2e657605
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-28 16:47:22 +0200

    feature(Editor): Add WorldEditor view

---
+++ src/Editor/Views/WorldEditor/WorldEditorView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Editor.Views.WorldEditor;
+
+public partial class WorldEditorView : UserControl
+{
+    public WorldEditorView()
+    {
+        InitializeComponent();
+    }
+}
