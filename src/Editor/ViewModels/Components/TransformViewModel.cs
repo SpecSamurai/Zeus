11bd558fb29006aee6d4702fea61f635ae4ac311
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-30 12:25:00 +0200

    feature(Editor): Add Transform view model

---
+++ src/Editor/ViewModels/Components/TransformViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+using Editor.ViewModels.WorldEditor;
+
+namespace Editor.ViewModels.Components;
+
+public class TransformViewModel : ComponentViewModel
+{
+    public TransformViewModel(GameEntityViewModel owner)
+        : base(owner)
+    {
+    }
+
+    public Vector3 Position { get; set; }
+    public Vector3 Rotation { get; set; }
+    public Vector3 Scale { get; set; }
+}
