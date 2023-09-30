2ee05ba6de662e6ba283cd27e29fdfd668d676b7
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-30 12:25:42 +0200

    feature(Editor): Add Script view model

---
+++ src/Editor/ViewModels/Components/ScriptViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Editor.ViewModels.WorldEditor;
+
+namespace Editor.ViewModels.Components;
+
+public class ScriptViewModel : ComponentViewModel
+{
+    public ScriptViewModel(GameEntityViewModel owner)
+        : base(owner)
+    {
+    }
+
+    public string? Script { get; set; }
+}
