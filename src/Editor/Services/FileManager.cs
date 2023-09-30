9d8d5d143be4814f3f1ecbc3421afe97dfa3b894
Spec <dawid.spec.jachowicz@gmail.com>
2023-09-30 11:16:57 +0200

    feature(Editor): Add FileManager service

---
+++ src/Editor/Services/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using Editor.Models.Files;
+using Frameworks;
+
+namespace Editor.Services;
+
+public static class FileManager
+{
+    public static HierarchicalFolder GetFolder(string path) =>
+        Functional.NavigateHierarchically(
+            path,
+            Directory.GetDirectories,
+            (folderPath, children) => new HierarchicalFolder(folderPath)
+            {
+                Folders = children.ToList(),
+                Files = GetFiles(folderPath)
+            },
+            folderPath => new HierarchicalFolder(folderPath)
+            {
+                Files = GetFiles(folderPath)
+            });
+
+    private static List<ContentFile> GetFiles(string folderPath) =>
+        Directory
+            .GetFiles(folderPath)
+            .Select(filePath => new ContentFile(filePath))
+            .ToList();
+}
