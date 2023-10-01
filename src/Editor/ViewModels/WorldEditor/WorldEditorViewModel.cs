46b298ef39479ea3a6b33c205300f59514594ede
Spec <dawid.spec.jachowicz@gmail.com>
2023-10-01 11:44:40 +0200

    feature(Editor): Add WorldEditor view model

---
+++ src/Editor/ViewModels/WorldEditor/WorldEditorViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Editor.Services;
+using ReactiveUI;
+
+namespace Editor.ViewModels.WorldEditor;
+
+public class WorldEditorViewModel : ViewModelBase
+{
+    private ProjectViewModel _projectViewModel;
+    private GameEntityViewModel? _activeGameEntity;
+    private ContentViewModel? _contentViewModel;
+
+    public WorldEditorViewModel(ProjectViewModel projectViewModel)
+    {
+        _projectViewModel = projectViewModel;
+        var folder = FileManager.GetFolder(_projectViewModel.Path);
+        ContentViewModel = new(folder);
+    }
+
+    public ContentViewModel? ContentViewModel
+    {
+        get => _contentViewModel;
+        set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+    }
+
+    public GameEntityViewModel? ActiveGameEntity
+    {
+        get => _activeGameEntity;
+        set => this.RaiseAndSetIfChanged(ref _activeGameEntity, value);
+    }
+
+    public ProjectViewModel ProjectViewModel
+    {
+        get => _projectViewModel;
+        set => this.RaiseAndSetIfChanged(ref _projectViewModel, value);
+    }
+}
