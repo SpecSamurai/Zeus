34fee4a6b04b24bb58e9eb4867f0324c6915d939
Spec <dawid.spec.jachowicz@gmail.com>
2023-10-01 11:37:08 +0200

    feature(Editor): Add History view model

---
+++ src/Editor/ViewModels/WorldEditor/HistoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Editor.Models.History;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using ReactiveUI;
+
+namespace Editor.ViewModels.WorldEditor;
+
+public class HistoryViewModel : ViewModelBase
+{
+    public ObservableCollection<IHistoryCommand> UndoList => History.UndoList;
+    public ObservableCollection<IHistoryCommand> RedoList => History.RedoList;
+
+    public ReactiveCommand<Unit, Unit> ClearHistoryCommand { get; } = ReactiveCommand.Create(History.Reset);
+}
