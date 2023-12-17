7075693ff137a5107ee85c57c031f63a1544918c
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-17 14:08:02 +0100

    feature(Editor): Add DesignData class to constants

---
+++ src/Editor/Constants/DesignData.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Editor.ViewModels.Projects;
+
+namespace Editor.Constants;
+
+public static class DesignData
+{
+    public static readonly ProjectTileViewModel ProjectTileViewModel = new()
+    {
+        Image = new Bitmap(AssetLoader.Open(new Uri("avares://Editor/Assets/DesignTime/Placeholder100x100.png"))),
+        Name = nameof(ProjectTileViewModel.Name)
+    };
+}
