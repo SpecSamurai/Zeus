4112cdef7117db5c8a2a881d09aa9d51d9b92dca
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-17 14:09:32 +0100

    feature(Editor): Add ProjectTileView

---
+++ src/Editor/Views/Projects/ProjectTileView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Editor.Views.Projects;
+
+public partial class ProjectTileView : UserControl
+{
+    public ProjectTileView()
+    {
+        InitializeComponent();
+    }
+}
