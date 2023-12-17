579257a9d023d137728648f88c8ad397892dfcba
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-17 14:06:19 +0100

    feature(Editor): Add ProjectTileViewModel

---
+++ src/Editor/ViewModels/Projects/ProjectTileViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Media.Imaging;
+
+namespace Editor.ViewModels.Projects;
+
+public class ProjectTileViewModel : ViewModelBase
+{
+    public required Bitmap Image { get; init; }
+    public required string Name { get; init; }
+}
