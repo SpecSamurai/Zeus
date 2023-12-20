5fdfe60daca7d8d4cb4ad49f10a90b8e3e041d43
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-20 21:46:08 +0100

    feature(Editor): Add CreateProjectView

---
+++ src/Editor/Views/StartPage/CreateProjectView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.ReactiveUI;
+using Editor.ViewModels.StartPage;
+using ReactiveUI;
+
+namespace Editor.Views.StartPage;
+
+public partial class CreateProjectView : ReactiveUserControl<CreateProjectViewModel>
+{
+    public CreateProjectView()
+    {
+        this.WhenActivated(disposables => { });
+        InitializeComponent();
+    }
+}
