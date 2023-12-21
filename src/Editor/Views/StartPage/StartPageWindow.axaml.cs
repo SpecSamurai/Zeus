8cc79b5208a5a3e7699b4c43d6d9e1f8ca8844ad
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-21 17:57:22 +0100

    feature(Editor): Add StartPageWindow

---
+++ src/Editor/Views/StartPage/StartPageWindow.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.ReactiveUI;
+using Editor.ViewModels.StartPage;
+using ReactiveUI;
+
+namespace Editor.Views.StartPage;
+
+public partial class StartPageWindow : ReactiveWindow<StartPageViewModel>
+{
+    public StartPageWindow()
+    {
+        this.WhenActivated(disposables => { });
+        InitializeComponent();
+    }
+}
