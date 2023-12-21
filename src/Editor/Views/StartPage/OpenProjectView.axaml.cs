171d246213e325c3c4dbd82b9c34dae2ca382853
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-21 07:16:36 +0100

    feature(Editor): Add OpenProjectView

---
+++ src/Editor/Views/StartPage/OpenProjectView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.ReactiveUI;
+using Editor.ViewModels.StartPage;
+using ReactiveUI;
+
+namespace Editor.Views.StartPage;
+
+public partial class OpenProjectView : ReactiveUserControl<OpenProjectViewModel>
+{
+    public OpenProjectView()
+    {
+        this.WhenActivated(disposables => { });
+        InitializeComponent();
+    }
+}
