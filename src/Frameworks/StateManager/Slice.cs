82c5280716b42e7615c0ebf6f1a962a8d079de09
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-31 14:01:10 +0100

    feature(Frameworks): Add Slice record

---
+++ src/Frameworks/StateManager/Slice.cs
@@ -0,0 +1,9 @@
+﻿using Frameworks.StateManager.Delegates;
+
+namespace Frameworks.StateManager;
+
+public record Slice<TState>(
+    string Name,
+    TState InitialState,
+    Reducer Reducer,
+    IReadOnlyList<ActionCreator> ActionCreators);
