c5f6389254de7ff47e0d9a3096d415b54625e662
Spec <dawid.spec.jachowicz@gmail.com>
2023-12-31 14:24:46 +0100

    feature(Frameworks): Add Store static factories and unit tests

---
+++ tests/Frameworks.Tests/StateManager/Fakes/TestState.cs
@@ -0,0 +1,7 @@
+﻿namespace Frameworks.Tests.StateManager.Fakes;
+
+public record TestState
+{
+    public string? Value1 { get; init; } = nameof(Value1);
+    public string? Value2 { get; init; } = nameof(Value2);
+}
