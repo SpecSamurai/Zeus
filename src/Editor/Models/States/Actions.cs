95ae587359b80270b89d2c8af69b2ba2dff9cf8b
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-09 10:26:21 +0100

    feature(Editor): Add History actions

---
+++ src/Editor/Models/States/Actions.cs
@@ -0,0 +1,17 @@
+﻿using Editor.Models.History;
+using Frameworks.StateManager;
+
+namespace Editor.Models.States;
+
+public static class Actions
+{
+    public static class History
+    {
+        public const string Slice = nameof(History);
+
+        public static readonly ActionCreator<IHistoryCommand> Add = Store.CreateAction<IHistoryCommand>($"{Slice}/{nameof(Add)}");
+        public static readonly ActionCreator Undo = Store.CreateAction($"{Slice}/{nameof(Undo)}");
+        public static readonly ActionCreator Redo = Store.CreateAction($"{Slice}/{nameof(Redo)}");
+        public static readonly ActionCreator Reset = Store.CreateAction($"{Slice}/{nameof(Reset)}");
+    }
+}
