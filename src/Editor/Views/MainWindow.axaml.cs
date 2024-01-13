f66213394fed057eb55e25d044e55850f0bf4034
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-13 18:06:21 +0100

    refactor(Editor): Refactor MainWindow to use StartPage window

--- src/Editor/Views/MainWindow.axaml.cs
+++ src/Editor/Views/MainWindow.axaml.cs
@@ -1,10 +1,9 @@
 using Avalonia.Interactivity;
 using Avalonia.ReactiveUI;
-using Editor.Extensions;
 using Editor.ViewModels;
-using Editor.ViewModels.ProjectBrowser;
+using Editor.ViewModels.StartPage;
 using Editor.ViewModels.WorldEditor;
-using Editor.Views.ProjectBrowser;
+using Editor.Views.StartPage;
 using ReactiveUI;
 using System.Reactive;
 
@@ -15,16 +14,16 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     public MainWindow()
     {
         InitializeComponent();
-        this.WhenActivated(action => action(ViewModel!.ShowProjectBrowserDialog.RegisterHandler(ShowProjectBrowserDialogAsync)));
+        this.WhenActivated(action => action(ViewModel!.ShowProjectBrowserDialog.RegisterHandler(ShowStartPageDialogAsync)));
         Loaded += ShowProjectBrowserDialogOnLoaded;
-
-        this.SetDesignDataContext<MainWindowViewModel>();
     }
 
-    private async Task ShowProjectBrowserDialogAsync(InteractionContext<ProjectBrowserWindowViewModel, ProjectViewModel?> interaction)
+    private async Task ShowStartPageDialogAsync(InteractionContext<StartPageViewModel, ProjectViewModel?> interaction)
     {
-        var dialog = new ProjectBrowserWindow();
-        dialog.DataContext = interaction.Input;
+        var dialog = new StartPageWindow
+        {
+            DataContext = interaction.Input
+        };
 
         var result = await dialog.ShowDialog<ProjectViewModel?>(this);
         interaction.SetOutput(result);
