243b48ddb5b5cb31cb251c26f66764ce85dc537b
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-13 18:07:48 +0100

    refactor(Editor): Delete SetDesignDataContext from SceneView

--- src/Editor/Views/WorldEditor/SceneView.axaml.cs
+++ src/Editor/Views/WorldEditor/SceneView.axaml.cs
@@ -1,6 +1,4 @@
 using Avalonia.Controls;
-using Editor.Extensions;
-using Editor.ViewModels.WorldEditor;
 
 namespace Editor.Views.WorldEditor;
 
@@ -9,6 +7,5 @@ public partial class SceneView : UserControl
     public SceneView()
     {
         InitializeComponent();
-        this.SetDesignDataContext<WorldEditorViewModel>();
     }
 }
