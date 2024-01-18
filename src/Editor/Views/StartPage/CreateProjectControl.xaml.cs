ba932b802ca069c61bb1f38b0c4775100b75b87c
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-18 21:42:12 +0100

    refactor(Editor): Remove usings

--- src/Editor/Views/StartPage/CreateProjectControl.xaml.cs
+++ src/Editor/Views/StartPage/CreateProjectControl.xaml.cs
@@ -1,28 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace Editor.Views.StartPage
+namespace Editor.Views.StartPage;
+
+public partial class CreateProjectControl : UserControl
 {
-    /// <summary>
-    /// Interaction logic for CreateProjectControl.xaml
-    /// </summary>
-    public partial class CreateProjectControl : UserControl
+    public CreateProjectControl()
     {
-        public CreateProjectControl()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
