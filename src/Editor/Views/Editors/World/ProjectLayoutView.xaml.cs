02c9cb7f5511660ee38e640cf71a8ee26895f247
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-20 12:21:37 +0100

    feature(Editor): Add World Editor views

---
+++ src/Editor/Views/Editors/World/ProjectLayoutView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Zeus.Views.Editors;
+
+public partial class ProjectLayoutView : UserControl
+{
+    public ProjectLayoutView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Editor/Views/Editors/World/WorldEditorView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Zeus.Views.Editors;
+
+public partial class WorldEditorView : UserControl
+{
+    public WorldEditorView()
+    {
+        InitializeComponent();
+    }
+}
