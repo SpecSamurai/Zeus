9fade12030630217e392cca362f362cb15d2563b
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-20 08:18:10 +0100

    feature(Editor): Add FileSystem constants

---
+++ src/Editor/Constants/FileSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Editor.Constants;
+
+public static class FileSystem
+{
+    public static readonly string DefaultProjectsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Zeus");
+
+    public const string SystemFolderName = ".Zeus";
+    public const string ContentFolderName = "Content";
+    public const string GameCodeFolderName = "GameCode";
+
+    public const string ProjectFileExtension = ".zeus";
+
+    public const string ProjectIconFileName = "Icon.png";
+    public const string ProjectScreenshotFileName = "Screenshot.png";
+    public const string ProjectFileName = $"project{ProjectFileExtension}";
+}
