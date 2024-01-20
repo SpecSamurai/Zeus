76a2aab33249d116d1a8f165e648a3491a5d9dc1
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-20 08:17:29 +0100

    feature(Editor): Add ViewModelBase

---
+++ src/Editor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace Editor.ViewModels;
+
+[DataContract(IsReference = true)]
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
