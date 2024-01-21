30f910fc3477ba0d16792cc23352e71eefd2a623
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-21 17:31:59 +0100

    feature(Editor): Add Logger

---
+++ src/Editor/Logging/LogEntry.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Zeus.Logging;
+
+public class LogEntry
+{
+    public LogEntry(LogLevel logLevel, string message, string file, string caller, int line)
+    {
+        DateTime = DateTime.Now;
+        LogLevel = logLevel;
+        Message = message;
+        File = Path.GetFileName(file);
+        Caller = caller;
+        Line = line;
+    }
+
+    public DateTime DateTime { get; }
+    public LogLevel LogLevel { get; }
+    public string Message { get; }
+    public string File { get; }
+    public string Caller { get; }
+    public int Line { get; }
+    public string MetaData => $"{File}: {Caller} ({Line})";
+}
---
+++ src/Editor/Logging/LogLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Zeus.Logging;
+
+[Flags]
+public enum LogLevel
+{
+    Info = 0x01,
+    Warning = 0x02,
+    Error = 0x04,
+}
