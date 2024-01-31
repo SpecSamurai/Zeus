329c5ac7dd20cef4000c38a611aeef398ea9f294
Spec <dawid.spec.jachowicz@gmail.com>
2024-01-31 09:36:01 +0100

    feature(Editor): Add MathUtil class

---
+++ src/Editor/Common/MathUtil.cs
@@ -0,0 +1,17 @@
+﻿namespace Zeus.Common;
+
+public static class MathUtil
+{
+    public static float Epsilon => 0.00001f;
+
+    public static bool IsTheSameAs(this float a, float b)
+    {
+        return Math.Abs(a - b) < Epsilon;
+    }
+
+    public static bool IsTheSameAs(this float? a, float? b)
+    {
+        if(!a.HasValue || !b.HasValue) return false;
+        return Math.Abs(a.Value - b.Value) < Epsilon;
+    }
+}
