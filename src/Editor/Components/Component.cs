081549ee9aefdd0abe0236840655d71ea68afba1
Spec <dawid.spec.jachowicz@gmail.com>
2024-02-06 21:39:03 +0100

    feature(Editor): Add Component class

---
+++ src/Editor/Components/Component.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+using Zeus.Common;
+
+namespace Zeus.Components;
+
+[DataContract]
+public abstract class Component : ViewModelBase
+{
+    public Component(GameEntity owner)
+    {
+        Owner = owner;
+    }
+
+    [DataMember] public GameEntity Owner { get; private set; }
+}
