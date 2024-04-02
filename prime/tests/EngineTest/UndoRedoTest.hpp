c1ff8cdca1f148025a2203b7044fea9c4b3d8156
Spec <dawid.spec.jachowicz@gmail.com>
2024-04-02 07:39:45 +0200

    Move DotNet Editor to separate folder

---
+++ prime/tests/EngineTest/UndoRedoTest.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Test.hpp"
+
+class UndoRedoTest : public Test
+{
+public:
+    bool Initialize() override
+    {
+        return true;
+    }
+
+    void Run() override
+    {
+    }
+
+    void Shutdown() override
+    {
+    }
+};
