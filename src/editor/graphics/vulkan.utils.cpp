c8efdc4a07609b49eeb11b0c93c603426087c075
Spec <dawid.spec.jachowicz@gmail.com>
2024-04-23 16:36:09 +0200

    Add Vulkan utils - Add implementation for createDebugUtilsMessengerEXT and destroyDebugUtilsMessengerEXT

---
+++ src/editor/graphics/vulkan.utils.cpp
@@ -0,0 +1,41 @@
+#include "vulkan.utils.hpp"
+
+#include <vulkan/vulkan_core.h>
+
+namespace Zeus
+{
+VkResult createDebugUtilsMessengerEXT(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pMessenger)
+{
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+        instance,
+        "vkCreateDebugUtilsMessengerEXT");
+
+    if (func != nullptr)
+    {
+        return func(instance, pCreateInfo, pAllocator, pMessenger);
+    }
+    else
+    {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+
+void destroyDebugUtilsMessengerEXT(
+    VkInstance instance,
+    VkDebugUtilsMessengerEXT messenger,
+    const VkAllocationCallbacks* pAllocator)
+{
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+        instance,
+        "vkDestroyDebugUtilsMessengerEXT");
+
+    if (func != nullptr)
+    {
+        func(instance, messenger, pAllocator);
+    }
+}
+}
---
+++ src/editor/graphics/vulkan.utils.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vulkan/vulkan_core.h>
+
+namespace Zeus
+{
+VkResult createDebugUtilsMessengerEXT(
+    VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pMessenger);
+
+void destroyDebugUtilsMessengerEXT(
+    VkInstance instance,
+    VkDebugUtilsMessengerEXT messenger,
+    const VkAllocationCallbacks* pAllocator);
+};
