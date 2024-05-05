b16a8e549b82c1ddb69d21a5312ce5e6079af259
Spec <dawid.spec.jachowicz@gmail.com>
2024-05-05 21:03:02 +0200

    Move vulkan_settings to vulkan directory and add debug configuration

---
+++ src/editor/graphics/vulkan/vulkan_settings.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include <cstdint>
+#include <vector>
+
+namespace Zeus
+{
+inline constexpr std::uint32_t APPLICATION_VERSION{VK_MAKE_VERSION(0, 1, 0)};
+
+inline const std::vector<const char*> DEVICE_EXTENSIONS = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+};
+
+inline constexpr int MAX_FRAMES_IN_FLIGHT{2};
+
+inline const std::vector<const char*> VALIDATION_LAYERS = {
+    "VK_LAYER_KHRONOS_validation",
+};
+
+constexpr VkDebugUtilsMessageSeverityFlagsEXT MESSAGE_SEVERITY{
+    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+    // VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+    VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+    VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT};
+
+constexpr VkDebugUtilsMessageTypeFlagsEXT MESSAGE_TYPE{
+    VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+    VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+    VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT};
+}
