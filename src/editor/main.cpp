4421afc7aeef0cca836860cf004cd2e418623f80
Spec <dawid.spec.jachowicz@gmail.com>
2024-05-10 16:07:59 +0200

    Remove unnecessary constants from Editor main

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -1,13 +1,9 @@
 #include "Application.hpp"
 
-constexpr int WIDTH{800};
-constexpr int HEIGHT{600};
-constexpr const char* TITLE{"Zeus"};
-
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
     Zeus::Application app;
-    app.Run(WIDTH, HEIGHT, TITLE);
+    app.Run();
 
     return 0;
 }
