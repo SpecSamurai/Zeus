23f9887ffa4c5fa7d793cfa3d59333313e8ec93a
Spec <dawid.spec.jachowicz@gmail.com>
2024-05-25 14:36:55 +0200

    Add comment to vulkan_framebuffer

--- src/editor/graphics/vulkan/vulkan_framebuffer.cpp
+++ src/editor/graphics/vulkan/vulkan_framebuffer.cpp
@@ -17,6 +17,17 @@ bool createVkFramebuffer(
     const VkExtent2D& swapChainExtent,
     VkFramebuffer& framebuffer)
 {
+    // Array of image views representing all attachments used in a framebuffer
+    // and render pass. Each element in this array (each image view) corresponds
+    // to each attachment in a render pass.
+    // each pAttachments array’s element corresponds directly to an attachment
+    // in a render pass description structure.
+    // But we may use a framebuffer not only with the specified render pass but
+    // also with all render passes that are compatible with the one specified.
+    // Compatible render passes, in general, must have the same number of
+    // attachments and corresponding attachments must have the same format and
+    // number of samples. But image layouts (initial, final, and for each
+    // subpass) may differ and doesn’t involve render pass compatibility.
     VkFramebufferCreateInfo framebufferInfo{
         .sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
         .renderPass = renderPass,
@@ -27,6 +38,9 @@ bool createVkFramebuffer(
         .layers = 1,
     };
 
+    // The framebuffer specifies what images are used as attachments on which
+    // the render pass operates. We can say that it translates image (image
+    // view) into a given attachment.
     VkResult result{
         vkCreateFramebuffer(device, &framebufferInfo, nullptr, &framebuffer)
     };
