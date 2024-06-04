1bdda0a7284354b0a053c084dc58f3fa2ae89743
Spec <dawid.spec.jachowicz@gmail.com>
2024-06-04 09:16:15 +0200

    Add memory helpers

---
+++ src/engine/memory/memory.cpp
@@ -0,0 +1,31 @@
+#include "memory.hpp"
+
+#include <cstddef>
+#include <cstdint>
+
+namespace Zeus
+{
+// formula increase an offset (address) to a next address aligned to the
+// alignment formula makes sense only when alignment is a power of 2
+std::size_t alignForwardAdjustment(
+    const void* const ptr,
+    const std::size_t& alignment) noexcept
+{
+    const auto offset{ reinterpret_cast<std::uintptr_t>(ptr) };
+    const auto aligned{ (offset - 1u + alignment) & -alignment };
+
+    return aligned - offset;
+}
+
+void* addPtr(const void* const ptr, const std::uintptr_t& offset) noexcept
+{
+    return reinterpret_cast<void*>(
+        reinterpret_cast<std::uintptr_t>(ptr) + offset);
+}
+
+void* subPtr(const void* const ptr, const std::uintptr_t& offset) noexcept
+{
+    return reinterpret_cast<void*>(
+        reinterpret_cast<std::uintptr_t>(ptr) - offset);
+}
+}
---
+++ src/engine/memory/memory.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace Zeus
+{
+std::size_t alignForwardAdjustment(
+    const void* const ptr,
+    const std::size_t& alignment) noexcept;
+
+template <typename T>
+std::size_t alignForwardAdjustmentWithHeader(
+    const void* const ptr,
+    const std::size_t& alignment) noexcept
+{
+    auto fixedAlignment{ alignof(T) > alignment ? alignof(T) : alignment };
+
+    std::size_t adjustment =
+        sizeof(T) +
+        alignForwardAdjustment(addPtr(ptr, sizeof(T)), fixedAlignment);
+
+    return adjustment;
+}
+
+void* addPtr(const void* const ptr, const std::uintptr_t& offset) noexcept;
+
+void* subPtr(const void* const ptr, const std::uintptr_t& offset) noexcept;
+}
