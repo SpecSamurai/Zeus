d2608db0d9a5f9e6fa5b00a2282ea75157df3f43
Spec <dawid.spec.jachowicz@gmail.com>
2024-07-07 11:26:53 +0200

    Move glfw callbacks to VulkanContext

--- src/editor/graphics/glfw_utils.cpp
+++ src/editor/graphics/glfw_utils.cpp
@@ -12,52 +12,6 @@ void glfwErrorCallback(int error, const char* description)
     Zeus::error("Error {}: {}", error, description);
 }
 
-inline void glfwKeyCallback(
-    GLFWwindow* window,
-    int key,
-    [[maybe_unused]] int scancode,
-    int action,
-    [[maybe_unused]] int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-inline void glfwCursorPositionCallback(
-    [[maybe_unused]] GLFWwindow* window,
-    [[maybe_unused]] double xpos,
-    [[maybe_unused]] double ypos)
-{
-    //   bool firstMouse = true;
-    //   float x = static_cast<float>(xpos);
-    //   float y = static_cast<float>(ypos);
-
-    //   if (firstMouse)
-    //   {
-    //       lastX = xpos;
-    //       lastY = ypos;
-    //       firstMouse = false;
-    //   }
-
-    //   float xoffset = xpos - lastX;
-    //   float yoffset =
-    //       lastY - ypos; // reversed since y-coordinates go from bottom to
-    //       top
-
-    //   lastX = xpos;
-    //   lastY = ypos;
-
-    // trace("Mouse position x:{}, y:{}", xpos, ypos);
-}
-
-inline void glfwScrollCallback(
-    [[maybe_unused]] GLFWwindow* window,
-    double xoffset,
-    double yoffset)
-{
-    trace("Mouse scroll x:{}, y:{}", xoffset, yoffset);
-}
-
 GLFWwindow* createGlfwWindow(int width, int height, const char* title)
 {
 #ifndef NDEBUG
@@ -82,14 +36,6 @@ GLFWwindow* createGlfwWindow(int width, int height, const char* title)
         return nullptr;
     }
 
-    // glfwSetKeyCallback(window, glfwKeyCallback);
-    // glfwSetCursorPosCallback(window, glfwCursorPositionCallback);
-    // glfwSetScrollCallback(window, glfwScrollCallback);
-
-    // glfwSetWindowUserPointer(window, this);
-    // glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
-    //
-    // lastTime = glfwGetTime();
     return window;
 }
 }
--- src/editor/graphics/vulkan/VulkanContext.hpp
+++ src/editor/graphics/vulkan/VulkanContext.hpp
@@ -20,12 +20,67 @@
 
 namespace Zeus
 {
+inline void glfwKeyCallback(
+    GLFWwindow* window,
+    int key,
+    [[maybe_unused]] int scancode,
+    int action,
+    [[maybe_unused]] int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+inline void glfwCursorPositionCallback(
+    [[maybe_unused]] GLFWwindow* window,
+    [[maybe_unused]] double xpos,
+    [[maybe_unused]] double ypos)
+{
+    //   bool firstMouse = true;
+    //   float x = static_cast<float>(xpos);
+    //   float y = static_cast<float>(ypos);
+
+    //   if (firstMouse)
+    //   {
+    //       lastX = xpos;
+    //       lastY = ypos;
+    //       firstMouse = false;
+    //   }
+
+    //   float xoffset = xpos - lastX;
+    //   float yoffset =
+    //       lastY - ypos; // reversed since y-coordinates go from bottom to
+    //       top
+
+    //   lastX = xpos;
+    //   lastY = ypos;
+
+    // trace("Mouse position x:{}, y:{}", xpos, ypos);
+}
+
+inline void glfwScrollCallback(
+    [[maybe_unused]] GLFWwindow* window,
+    double xoffset,
+    double yoffset)
+{
+    trace("Mouse scroll x:{}, y:{}", xoffset, yoffset);
+}
+
 class VulkanContext
 {
 public:
     void Init()
     {
         window = createGlfwWindow(800, 600, "Zeus");
+        // glfwSetKeyCallback(window, glfwKeyCallback);
+        // glfwSetCursorPosCallback(window, glfwCursorPositionCallback);
+        // glfwSetScrollCallback(window, glfwScrollCallback);
+
+        // glfwSetWindowUserPointer(window, this);
+        // glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+
+        // lastTime = glfwGetTime();
+
         createVkInstance(instance);
 #ifndef NDEBUG
         createVkDebugUtilsMessengerEXT(instance, debugUtilsMessenger);
