27382bb38eb06083c0d51f8e0c0e4a2a2962d913
Spec <dawid.spec.jachowicz@gmail.com>
2024-07-17 08:49:45 +0200

    Add Matrix3x3 and corresponding unit tests

---
+++ src/engine/math/Matrix3x3.hpp
@@ -0,0 +1,185 @@
+#pragma once
+
+#include "Vector3.hpp"
+
+#include <cassert>
+#include <cstddef>
+
+namespace Zeus
+{
+template <typename T>
+class Matrix3x3
+{
+public:
+    Matrix3x3()
+        : m_data{ Vector3<T>(0, 0, 0),
+                  Vector3<T>(0, 0, 0),
+                  Vector3<T>(0, 0, 0) }
+    {
+    }
+
+    Matrix3x3(
+        // clang-format off
+        const T& x0, const T& y0, const T& z0,
+        const T& x1, const T& y1, const T& z1,
+        const T& x2, const T& y2, const T& z2)
+        // clang-format on
+        : m_data{ Vector3<T>(x0, y0, z0),
+                  Vector3<T>(x1, y1, z1),
+                  Vector3<T>(x2, y2, z2) }
+    {
+    }
+
+    Matrix3x3(Vector3<T> column1, Vector3<T> column2, Vector3<T> column3)
+        : m_data{ column1, column2, column3 }
+    {
+    }
+
+    Matrix3x3(T scalar)
+        : m_data{ Vector3<T>(scalar, 0, 0),
+                  Vector3<T>(0, scalar, 0),
+                  Vector3<T>(0, 0, scalar) }
+    {
+    }
+
+    constexpr Vector3<T>& operator[](const std::size_t i)
+    {
+        assert(0 <= i && i < size());
+        return m_data[i];
+    }
+
+    constexpr Vector3<T> const& operator[](const std::size_t i) const
+    {
+        assert(0 <= i && i < size());
+        return m_data[i];
+    }
+
+private:
+    constexpr std::size_t size() const
+    {
+        return 3;
+    }
+
+private:
+    Vector3<T> m_data[3];
+};
+
+template <typename T>
+constexpr Matrix3x3<T> operator+(
+    const Matrix3x3<T>& left,
+    const Matrix3x3<T>& right)
+{
+    return Matrix3x3<T>(
+        left[0] + right[0],
+        left[1] + right[1],
+        left[2] + right[2]);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator+(const T& scalar, const Matrix3x3<T>& right)
+{
+    return Matrix3x3<T>(
+        scalar + right[0],
+        scalar + right[1],
+        scalar + right[2]);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator+(const Matrix3x3<T>& left, const T& scalar)
+{
+    return Matrix3x3<T>(left[0] + scalar, left[1] + scalar, left[2] + scalar);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator-(
+    const Matrix3x3<T>& left,
+    const Matrix3x3<T>& right)
+{
+    return Matrix3x3<T>(
+        left[0] - right[0],
+        left[1] - right[1],
+        left[2] - right[2]);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator-(const Matrix3x3<T>& left, const T& scalar)
+{
+    return Matrix3x3<T>(left[0] - scalar, left[1] - scalar, left[2] - scalar);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator-(const T& scalar, const Matrix3x3<T>& right)
+{
+    return Matrix3x3<T>(
+        scalar - right[0],
+        scalar - right[1],
+        scalar - right[2]);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator*(const Matrix3x3<T>& left, const T& scalar)
+{
+    return Matrix3x3<T>(left[0] * scalar, left[1] * scalar, left[2] * scalar);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator*(const T& scalar, const Matrix3x3<T>& right)
+{
+    return Matrix3x3<T>(
+        scalar * right[0],
+        scalar * right[1],
+        scalar * right[2]);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator*(
+    const Matrix3x3<T>& left,
+    const Matrix3x3<T>& right)
+{
+    const Vector3<T>& ColA0{ left[0] };
+    const Vector3<T>& ColA1{ left[1] };
+    const Vector3<T>& ColA2{ left[2] };
+
+    const Vector3<T>& ColB0{ right[0] };
+    const Vector3<T>& ColB1{ right[1] };
+    const Vector3<T>& ColB2{ right[2] };
+
+    Matrix3x3<T> result;
+
+    result[0] = ColA2 * ColB0.z + ColA1 * ColB0.y + ColA0 * ColB0.x;
+    result[1] = ColA2 * ColB1.z + ColA1 * ColB1.y + ColA0 * ColB1.x;
+    result[2] = ColA2 * ColB2.z + ColA1 * ColB2.y + ColA0 * ColB2.x;
+
+    return result;
+}
+
+template <typename T>
+constexpr Vector3<T> operator*(
+    const Matrix3x3<T>& left,
+    const Vector3<T>& right)
+{
+
+    return Vector3<T>(
+        // clang-format off
+        left[0][0] * right[0] + left[1][0] * right[1] + left[2][0] * right[2],
+        left[0][1] * right[0] + left[1][1] * right[1] + left[2][1] * right[2],
+        left[0][2] * right[0] + left[1][2] * right[1] + left[2][2] * right[2]
+        // clang-format on
+    );
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator/(const Matrix3x3<T>& left, const T& scalar)
+{
+    return Matrix3x3<T>(left[0] / scalar, left[1] / scalar, left[2] / scalar);
+}
+
+template <typename T>
+constexpr Matrix3x3<T> operator/(const T& scalar, const Matrix3x3<T>& right)
+{
+    return Matrix3x3<T>(
+        scalar / right[0],
+        scalar / right[1],
+        scalar / right[2]);
+}
+}
---
+++ tests/engine/math/Matrix3x3Test.cpp
@@ -0,0 +1,330 @@
+#include <math/Matrix3x3.hpp>
+#include <math/Vector3.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(Matrix3x3Test, Constructor_ArraySubscriptOperator)
+{
+    auto sut = Zeus::Matrix3x3(
+        // clang-format off
+        0.f, 0.f, 0.f,
+        0.f, 0.f, 0.f,
+        0.f, 0.f, 0.f
+        // clang-format on
+    );
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(0.f, sut[i][j]);
+
+    float temp{ 1.f };
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            sut[i][j] = temp++;
+
+    temp = 1.f;
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(temp++, sut[i][j]);
+}
+
+TEST(Matrix3x3Test, Constructor_Scalar)
+{
+    auto sut = Zeus::Matrix3x3(1.f);
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            if (i == j)
+                EXPECT_EQ(1.f, sut[i][j]);
+            else
+                EXPECT_EQ(0, sut[i][j]);
+}
+
+TEST(Matrix3x3Test, BinaryAdditionOperator_Matrices)
+{
+    auto sut1 = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 2.f, 3.f,
+        4.f, 5.f, 6.f,
+        7.f, 8.f, 9.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Matrix3x3(
+        // clang-format off
+        10.f, 11.f, 12.f,
+        13.f, 14.f, 15.f,
+        16.f, 17.f, 18.f
+        // clang-format on
+    );
+
+    auto expected = Zeus::Matrix3x3(
+        // clang-format off
+        11.f, 13.f, 15.f,
+        17.f, 19.f, 21.f,
+        23.f, 25.f, 27.f
+        // clang-format on
+    );
+
+    auto actual = sut1 + sut2;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected[i][j], actual[i][j]);
+}
+
+TEST(Matrix3x3Test, BinaryAdditionOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 2.f, 3.f,
+        4.f, 5.f, 6.f,
+        7.f, 8.f, 9.f
+        // clang-format on
+    );
+
+    float scalar = 1.f;
+
+    auto expected = Zeus::Matrix3x3(
+        // clang-format off
+        2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f,
+        8.f, 9.f, 10.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar + sut;
+    auto actual2 = sut + scalar;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected[i][j], actual2[i][j]);
+}
+
+TEST(Matrix3x3Test, BinarySubtractionOperator_Matrices)
+{
+    auto sut1 = Zeus::Matrix3x3(
+        // clang-format off
+        9.f, 8.f, 7.f,
+        6.f, 5.f, 4.f,
+        3.f, 2.f, 1.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 2.f, 3.f,
+        4.f, 5.f, 6.f,
+        7.f, 8.f, 9.f
+        // clang-format on
+    );
+
+    auto expected1 = Zeus::Matrix3x3(
+        // clang-format off
+        8.f, 6.f, 4.f,
+        2.f, 0.f, -2.f,
+        -4.f, -6.f, -8.f
+        // clang-format on
+    );
+
+    auto expected2 = Zeus::Matrix3x3(
+        // clang-format off
+        -8.f, -6.f, -4.f,
+        -2.f, 0.f, 2.f,
+        4.f, 6.f, 8.f
+        // clang-format on
+    );
+
+    auto actual1 = sut1 - sut2;
+    auto actual2 = sut2 - sut1;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix3x3Test, BinarySubtractionOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 2.f, 3.f,
+        5.f, 6.f, 7.f,
+        9.f, 10.f, 11.f
+        // clang-format on
+    );
+
+    float scalar = 1.f;
+
+    auto expected1 = Zeus::Matrix3x3(
+        // clang-format off
+        0.f, -1.f, -2.f,
+        -4.f, -5.f, -6.f,
+        -8.f, -9.f, -10.f
+        // clang-format on
+    );
+
+    auto expected2 = Zeus::Matrix3x3(
+        // clang-format off
+        0.f, 1.f, 2.f,
+        4.f, 5.f, 6.f,
+        8.f, 9.f, 10.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar - sut;
+    auto actual2 = sut - scalar;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix3x3Test, BinaryMultiplicationOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 2.f, 3.f,
+        5.f, 6.f, 7.f,
+        9.f, 10.f, 11.f
+        // clang-format on
+    );
+
+    float scalar = 2.f;
+
+    auto expected = Zeus::Matrix3x3(
+        // clang-format off
+        2.f, 4.f, 6.f,
+        10.f, 12.f, 14.f,
+        18.f, 20.f, 22.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar * sut;
+    auto actual2 = sut * scalar;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected[i][j], actual2[i][j]);
+}
+
+TEST(Matrix3x3Test, BinaryDivisionOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix3x3(
+        // clang-format off
+        2.f, 4.f, 6.f,
+        10.f, 12.f, 14.f,
+        18.f, 20.f, 22.f
+        // clang-format on
+    );
+
+    float scalar = 2.f;
+
+    auto expected1 = Zeus::Matrix3x3(
+        // clang-format off
+        2.f / 2.f, 2.f / 4.f, 2.f / 6.f,
+        2.f / 10.f, 2.f / 12.f, 2.f / 14.f,
+        2.f / 18.f, 2.f / 20.f, 2.f / 22.f
+        // clang-format on
+    );
+
+    auto expected2 = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 2.f, 3.f,
+        5.f, 6.f, 7.f,
+        9.f, 10.f, 11.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar / sut;
+    auto actual2 = sut / scalar;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix3x3Test, BinaryMultiplicationOperator_MatrixWithMatrix)
+{
+    auto sut1 = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 4.f, 7.f,
+        2.f, 5.f, 8.f,
+        3.f, 6.f, 9.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Matrix3x3(
+        // clang-format off
+        10.f, 13.f, 16.f,
+        11.f, 14.f, 17.f,
+        12.f, 15.f, 18.f
+        // clang-format on
+    );
+
+    Zeus::Matrix3x3 expected1(
+        // clang-format off
+        84.f, 201.f, 318.f,
+        90.f, 216.f, 342.f,
+        96.f, 231.f, 366.f
+        // clang-format on
+    );
+
+    Zeus::Matrix3x3 expected2(
+        // clang-format off
+        138.f, 174.f, 210.f,
+        171.f, 216.f, 261.f,
+        204.f, 258.f, 312.f
+        // clang-format on
+    );
+
+    auto actual1 = sut1 * sut2;
+    auto actual2 = sut2 * sut1;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        for (std::size_t j{ 0 }; j < 3; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix3x3Test, BinaryMultiplicationOperator_MatrixWithVector)
+{
+    auto sut1 = Zeus::Matrix3x3(
+        // clang-format off
+        1.f, 4.f, 7.f,
+        2.f, 5.f, 8.f,
+        3.f, 6.f, 9.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Vector3(2.f, 4.f, 8.f);
+
+    auto expected = Zeus::Vector3(34.f, 76.f, 118.f);
+
+    auto actual = sut1 * sut2;
+
+    for (std::size_t i{ 0 }; i < 3; ++i)
+        EXPECT_EQ(expected[i], actual[i]);
+}
