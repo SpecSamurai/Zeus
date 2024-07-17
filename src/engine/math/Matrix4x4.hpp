12a5a8dad7a88f57d89fefe226df40ed36384259
Spec <dawid.spec.jachowicz@gmail.com>
2024-07-17 08:48:53 +0200

    Add Matrix4x4 and corresponding unit tests

---
+++ src/engine/math/Matrix4x4.hpp
@@ -0,0 +1,222 @@
+#pragma once
+
+#include "Vector4.hpp"
+
+#include <cassert>
+#include <cstddef>
+
+namespace Zeus
+{
+template <typename T>
+class Matrix4x4
+{
+public:
+    Matrix4x4()
+        : m_data{ Vector4<T>(0, 0, 0, 0),
+                  Vector4<T>(0, 0, 0, 0),
+                  Vector4<T>(0, 0, 0, 0),
+                  Vector4<T>(0, 0, 0, 0) }
+    {
+    }
+
+    Matrix4x4(
+        // clang-format off
+        const T& x0, const T& y0, const T& z0, const T& w0,
+        const T& x1, const T& y1, const T& z1, const T& w1,
+        const T& x2, const T& y2, const T& z2, const T& w2,
+        const T& x3, const T& y3, const T& z3, const T& w3)
+        // clang-format on
+        : m_data{ Vector4<T>(x0, y0, z0, w0),
+                  Vector4<T>(x1, y1, z1, w1),
+                  Vector4<T>(x2, y2, z2, w2),
+                  Vector4<T>(x3, y3, z3, w3) }
+    {
+    }
+
+    Matrix4x4(
+        Vector4<T> column1,
+        Vector4<T> column2,
+        Vector4<T> column3,
+        Vector4<T> column4)
+        : m_data{ column1, column2, column3, column4 }
+    {
+    }
+
+    Matrix4x4(T scalar)
+        : m_data{ Vector4<T>(scalar, 0, 0, 0),
+                  Vector4<T>(0, scalar, 0, 0),
+                  Vector4<T>(0, 0, scalar, 0),
+                  Vector4<T>(0, 0, 0, scalar) }
+    {
+    }
+
+    constexpr Vector4<T>& operator[](const std::size_t i)
+    {
+        assert(0 <= i && i < size());
+        return m_data[i];
+    }
+
+    constexpr Vector4<T> const& operator[](const std::size_t i) const
+    {
+        assert(0 <= i && i < size());
+        return m_data[i];
+    }
+
+private:
+    constexpr std::size_t size() const
+    {
+        return 4;
+    }
+
+private:
+    Vector4<T> m_data[4];
+};
+
+template <typename T>
+constexpr Matrix4x4<T> operator+(
+    const Matrix4x4<T>& left,
+    const Matrix4x4<T>& right)
+{
+    return Matrix4x4<T>(
+        left[0] + right[0],
+        left[1] + right[1],
+        left[2] + right[2],
+        left[3] + right[3]);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator+(const T& scalar, const Matrix4x4<T>& right)
+{
+    return Matrix4x4<T>(
+        scalar + right[0],
+        scalar + right[1],
+        scalar + right[2],
+        scalar + right[3]);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator+(const Matrix4x4<T>& left, const T& scalar)
+{
+    return Matrix4x4<T>(
+        left[0] + scalar,
+        left[1] + scalar,
+        left[2] + scalar,
+        left[3] + scalar);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator-(
+    const Matrix4x4<T>& left,
+    const Matrix4x4<T>& right)
+{
+    return Matrix4x4<T>(
+        left[0] - right[0],
+        left[1] - right[1],
+        left[2] - right[2],
+        left[3] - right[3]);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator-(const Matrix4x4<T>& left, const T& scalar)
+{
+    return Matrix4x4<T>(
+        left[0] - scalar,
+        left[1] - scalar,
+        left[2] - scalar,
+        left[3] - scalar);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator-(const T& scalar, const Matrix4x4<T>& right)
+{
+    return Matrix4x4<T>(
+        scalar - right[0],
+        scalar - right[1],
+        scalar - right[2],
+        scalar - right[3]);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator*(const Matrix4x4<T>& left, const T& scalar)
+{
+    return Matrix4x4<T>(
+        left[0] * scalar,
+        left[1] * scalar,
+        left[2] * scalar,
+        left[3] * scalar);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator*(const T& scalar, const Matrix4x4<T>& right)
+{
+    return Matrix4x4<T>(
+        scalar * right[0],
+        scalar * right[1],
+        scalar * right[2],
+        scalar * right[3]);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator*(
+    const Matrix4x4<T>& left,
+    const Matrix4x4<T>& right)
+{
+    const Vector4<T>& ColA0{ left[0] };
+    const Vector4<T>& ColA1{ left[1] };
+    const Vector4<T>& ColA2{ left[2] };
+    const Vector4<T>& ColA3{ left[3] };
+
+    const Vector4<T>& ColB0{ right[0] };
+    const Vector4<T>& ColB1{ right[1] };
+    const Vector4<T>& ColB2{ right[2] };
+    const Vector4<T>& ColB3{ right[3] };
+
+    Matrix4x4<T> result;
+
+    result[0] =
+        ColA3 * ColB0.w + ColA2 * ColB0.z + ColA1 * ColB0.y + ColA0 * ColB0.x;
+    result[1] =
+        ColA3 * ColB1.w + ColA2 * ColB1.z + ColA1 * ColB1.y + ColA0 * ColB1.x;
+    result[2] =
+        ColA3 * ColB2.w + ColA2 * ColB2.z + ColA1 * ColB2.y + ColA0 * ColB2.x;
+    result[3] =
+        ColA3 * ColB3.w + ColA2 * ColB3.z + ColA1 * ColB3.y + ColA0 * ColB3.x;
+
+    return result;
+}
+
+template <typename T>
+constexpr Vector4<T> operator*(
+    const Matrix4x4<T>& left,
+    const Vector4<T>& right)
+{
+    return Vector4<T>(
+        // clang-format off
+        left[0][0] * right[0] + left[1][0] * right[1] + left[2][0] * right[2] + left[3][0] * right[3],
+        left[0][1] * right[0] + left[1][1] * right[1] + left[2][1] * right[2] + left[3][1] * right[3],
+        left[0][2] * right[0] + left[1][2] * right[1] + left[2][2] * right[2] + left[3][2] * right[3],
+        left[0][3] * right[0] + left[1][3] * right[1] + left[2][3] * right[2] + left[3][3] * right[3]
+        // clang-format on
+    );
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator/(const Matrix4x4<T>& left, const T& scalar)
+{
+    return Matrix4x4<T>(
+        left[0] / scalar,
+        left[1] / scalar,
+        left[2] / scalar,
+        left[3] / scalar);
+}
+
+template <typename T>
+constexpr Matrix4x4<T> operator/(const T& scalar, const Matrix4x4<T>& right)
+{
+    return Matrix4x4<T>(
+        scalar / right[0],
+        scalar / right[1],
+        scalar / right[2],
+        scalar / right[3]);
+}
+}
---
+++ tests/engine/math/Matrix4x4Test.cpp
@@ -0,0 +1,352 @@
+#include <math/Matrix4x4.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(Matrix4x4Test, Constructor_ArraySubscriptOperator)
+{
+    auto sut = Zeus::Matrix4x4(
+        // clang-format off
+        0.f, 0.f, 0.f, 0.f,
+        0.f, 0.f, 0.f, 0.f,
+        0.f, 0.f, 0.f, 0.f,
+        0.f, 0.f, 0.f, 0.f
+        // clang-format on
+    );
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(0.f, sut[i][j]);
+
+    float temp{ 1.f };
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            sut[i][j] = temp++;
+
+    temp = 1.f;
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(temp++, sut[i][j]);
+}
+
+TEST(Matrix4x4Test, Constructor_Scalar)
+{
+    auto sut = Zeus::Matrix4x4(1.f);
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            if (i == j)
+                EXPECT_EQ(1.f, sut[i][j]);
+            else
+                EXPECT_EQ(0, sut[i][j]);
+}
+
+TEST(Matrix4x4Test, BinaryAdditionOperator_Matrices)
+{
+    auto sut1 = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f,
+        9.f, 10.f, 11.f, 12.f,
+        13.f, 14.f, 15.f, 16.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Matrix4x4(
+        // clang-format off
+        10.f, 11.f, 12.f, 13.f,
+        14.f, 15.f, 16.f, 17.f,
+        18.f, 19.f, 20.f, 21.f,
+        22.f, 23.f, 24.f, 25.f
+        // clang-format on
+    );
+
+    auto expected = Zeus::Matrix4x4(
+        // clang-format off
+        11.f, 13.f, 15.f, 17.f,
+        19.f, 21.f, 23.f, 25.f,
+        27.f, 29.f, 31.f, 33.f,
+        35.f, 37.f, 39.f, 41.f
+        // clang-format on
+    );
+
+    auto actual = sut1 + sut2;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected[i][j], actual[i][j]);
+}
+
+TEST(Matrix4x4Test, BinaryAdditionOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f,
+        9.f, 10.f, 11.f, 12.f,
+        13.f, 14.f, 15.f, 16.f
+        // clang-format on
+    );
+
+    float scalar = 1.f;
+
+    auto expected = Zeus::Matrix4x4(
+        // clang-format off
+        2.f, 3.f, 4.f, 5.f,
+        6.f, 7.f, 8.f, 9.f,
+        10.f, 11.f, 12.f, 13.f,
+        14.f, 15.f, 16.f, 17.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar + sut;
+    auto actual2 = sut + scalar;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected[i][j], actual2[i][j]);
+}
+
+TEST(Matrix4x4Test, BinarySubtractionOperator_Matrices)
+{
+    auto sut1 = Zeus::Matrix4x4(
+        // clang-format off
+        16.f, 15.f, 14.f, 13.f,
+        12.f, 11.f, 10.f, 9.f,
+        8.f, 7.f, 6.f, 5.f,
+        4.f, 3.f, 2.f, 1.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f,
+        9.f, 10.f, 11.f, 12.f,
+        13.f, 14.f, 15.f, 16.f
+        // clang-format on
+    );
+
+    auto expected1 = Zeus::Matrix4x4(
+        // clang-format off
+        15.f, 13.f, 11.f, 9.f,
+        7.f, 5.f, 3.f, 1.f,
+        -1.f, -3.f, -5.f, -7.f,
+        -9.f, -11.f, -13.f, -15.f
+        // clang-format on
+    );
+
+    auto expected2 = Zeus::Matrix4x4(
+        // clang-format off
+        -15.f, -13.f, -11.f, -9.f,
+        -7.f, -5.f, -3.f, -1.f,
+        1.f,3.f, 5.f, 7.f,
+        9.f, 11.f, 13.f, 15.f
+        // clang-format on
+    );
+
+    auto actual1 = sut1 - sut2;
+    auto actual2 = sut2 - sut1;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix4x4Test, BinarySubtractionOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f,
+        9.f, 10.f, 11.f, 12.f,
+        13.f, 14.f, 15.f, 16.f
+        // clang-format on
+    );
+
+    float scalar = 1.f;
+
+    auto expected1 = Zeus::Matrix4x4(
+        // clang-format off
+        0.f, -1.f, -2.f, -3.f,
+        -4.f, -5.f, -6.f, -7.f,
+        -8.f, -9.f, -10.f, -11.f,
+        -12.f, -13.f, -14.f, -15.f
+        // clang-format on
+    );
+
+    auto expected2 = Zeus::Matrix4x4(
+        // clang-format off
+        0.f, 1.f, 2.f, 3.f,
+        4.f, 5.f, 6.f, 7.f,
+        8.f, 9.f, 10.f, 11.f,
+        12.f, 13.f, 14.f, 15.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar - sut;
+    auto actual2 = sut - scalar;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix4x4Test, BinaryMultiplicationOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f,
+        9.f, 10.f, 11.f, 12.f,
+        13.f, 14.f, 15.f, 16.f
+        // clang-format on
+    );
+
+    float scalar = 2.f;
+
+    auto expected = Zeus::Matrix4x4(
+        // clang-format off
+        2.f, 4.f, 6.f, 8.f,
+        10.f, 12.f, 14.f, 16.f,
+        18.f, 20.f, 22.f, 24.f,
+        26.f, 28.f, 30.f, 32.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar * sut;
+    auto actual2 = sut * scalar;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected[i][j], actual2[i][j]);
+}
+
+TEST(Matrix4x4Test, BinaryDivisionOperator_MatrixWithScalar)
+{
+    auto sut = Zeus::Matrix4x4(
+        // clang-format off
+        2.f, 4.f, 6.f, 8.f,
+        10.f, 12.f, 14.f, 16.f,
+        18.f, 20.f, 22.f, 24.f,
+        26.f, 28.f, 30.f, 32.f
+        // clang-format on
+    );
+
+    float scalar = 2.f;
+
+    auto expected1 = Zeus::Matrix4x4(
+        // clang-format off
+        2.f / 2.f, 2.f / 4.f, 2.f / 6.f, 2.f / 8.f,
+        2.f / 10.f, 2.f / 12.f, 2.f / 14.f, 2.f / 16.f,
+        2.f / 18.f, 2.f / 20.f, 2.f / 22.f, 2.f / 24.f,
+        2.f / 26.f, 2.f / 28.f, 2.f / 30.f, 2.f / 32.f
+        // clang-format on
+    );
+
+    auto expected2 = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 2.f, 3.f, 4.f,
+        5.f, 6.f, 7.f, 8.f,
+        9.f, 10.f, 11.f, 12.f,
+        13.f, 14.f, 15.f, 16.f
+        // clang-format on
+    );
+
+    auto actual1 = scalar / sut;
+    auto actual2 = sut / scalar;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix4x4Test, BinaryMultiplicationOperator_MatrixWithMatrix)
+{
+    auto sut1 = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 5.f, 9.f, 13.f,
+        2.f, 6.f, 10.f, 14.f,
+        3.f, 7.f, 11.f, 15.f,
+        4.f, 8.f, 12.f, 16.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Matrix4x4(
+        // clang-format off
+        10.f, 14.f, 18.f, 22.f,
+        11.f, 15.f, 19.f, 23.f,
+        12.f, 16.f, 20.f, 24.f,
+        13.f, 17.f, 21.f, 25.f
+        // clang-format on
+    );
+
+    auto expected1 = Zeus::Matrix4x4(
+        // clang-format off
+        180.f, 436.f, 692.f, 948.f,
+        190.f, 462.f, 734.f, 1006.f,
+        200.f, 488.f, 776.f, 1064.f,
+        210.f, 514.f, 818.f, 1122.f
+        // clang-format on
+    );
+
+    auto expected2 = Zeus::Matrix4x4(
+        // clang-format off
+        342.f, 454.f, 566.f, 678.f,
+        388.f, 516.f, 644.f, 772.f,
+        434.f, 578.f, 722.f, 866.f,
+        480.f, 640.f, 800.f, 960.f
+        // clang-format on
+    );
+
+    auto actual1 = sut1 * sut2;
+    auto actual2 = sut2 * sut1;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected1[i][j], actual1[i][j]);
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        for (std::size_t j{ 0 }; j < 4; ++j)
+            EXPECT_EQ(expected2[i][j], actual2[i][j]);
+}
+
+TEST(Matrix4x4Test, BinaryMultiplicationOperator_MatrixWithVector)
+{
+    auto sut1 = Zeus::Matrix4x4(
+        // clang-format off
+        1.f, 5.f, 9.f, 13.f,
+        2.f, 6.f, 10.f, 14.f,
+        3.f, 7.f, 11.f, 15.f,
+        4.f, 8.f, 12.f, 16.f
+        // clang-format on
+    );
+
+    auto sut2 = Zeus::Vector4(2.f, 4.f, 8.f, 16.f);
+
+    auto expected = Zeus::Vector4(98.f, 218.f, 338.f, 458.f);
+
+    auto actual = sut1 * sut2;
+
+    for (std::size_t i{ 0 }; i < 4; ++i)
+        EXPECT_EQ(expected[i], actual[i]);
+}
