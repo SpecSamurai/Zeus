6dfd54c030508cdc03db602136ee25a2425d2567
Spec <dawid.spec.jachowicz@gmail.com>
2024-07-18 07:38:03 +0200

    Remove Application from main function

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -1,34 +1,24 @@
-#include "Application.hpp"
 #include "VulkanContext.hpp"
 
 #include <cstdlib>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-    bool testContext = 1;
-    if (testContext)
+    Zeus::VulkanContext vkContext;
+
+    vkContext.Init(800, 600, "Zeus");
+
+    while (!glfwWindowShouldClose(vkContext.window))
     {
-        Zeus::VulkanContext vkContext;
-
-        vkContext.Init(800, 600, "Zeus");
-
-        while (!glfwWindowShouldClose(vkContext.window))
-        {
-            glfwPollEvents();
-            vkContext.DrawFrame();
-            // lastTime = glfwGetTime();
-        }
-
-        // Wait for operations in a specific command queue to be finished.
-        // Can be used as a rudimentary synchronization
-        vkDeviceWaitIdle(vkContext.device.logicalDevice);
-        vkContext.Destroy();
+        glfwPollEvents();
+        vkContext.DrawFrame();
+        // lastTime = glfwGetTime();
     }
-    else
-    {
-        Zeus::Application app;
-        app.Run();
-    }
+
+    // Wait for operations in a specific command queue to be finished.
+    // Can be used as a rudimentary synchronization
+    vkDeviceWaitIdle(vkContext.device.logicalDevice);
+    vkContext.Destroy();
 
     return EXIT_SUCCESS;
 }
