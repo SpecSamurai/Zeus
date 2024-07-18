beecdd1bb72a69130d6a55b48093573fa8da22d4
Spec <dawid.spec.jachowicz@gmail.com>
2024-07-18 22:37:20 +0200

    Add empty constructor to Vector2, Vector3, Vector4

--- src/engine/math/Vector2.hpp
+++ src/engine/math/Vector2.hpp
@@ -9,6 +9,10 @@ template <typename T>
 class Vector2
 {
 public:
+    Vector2() : x{ 0 }, y{ 0 }
+    {
+    }
+
     Vector2(T x, T y) : x{ x }, y{ y }
     {
     }
--- src/engine/math/Vector3.hpp
+++ src/engine/math/Vector3.hpp
@@ -9,6 +9,10 @@ template <typename T>
 class Vector3
 {
 public:
+    Vector3() : x{ 0 }, y{ 0 }, z{ 0 }
+    {
+    }
+
     Vector3(T x, T y, T z) : x{ x }, y{ y }, z{ z }
     {
     }
