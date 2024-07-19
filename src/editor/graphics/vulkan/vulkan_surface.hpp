674ca997f7c15abea02cf3a43113d030c474e327
Spec <dawid.spec.jachowicz@gmail.com>
2024-07-19 12:17:34 +0200

    Refactor vulkan_surface

--- src/editor/graphics/vulkan/vulkan_surface.hpp
+++ src/editor/graphics/vulkan/vulkan_surface.hpp
@@ -2,16 +2,11 @@
 
 #define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
-#include "GLFW/glfw3.h"
-#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan_core.h>
 
 namespace Zeus
 {
-struct Window
-{
-    GLFWwindow* handle;
-};
-
 bool createVkSurfaceKHR(
     const VkInstance& instance,
     GLFWwindow* window,
