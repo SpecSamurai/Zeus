42d3d54e4fe561bca14dc28b9677cbc1ae4aa395
Spec <dawid.spec.jachowicz@gmail.com>
2024-07-19 12:06:55 +0200

    Refactor vulkan_settings

--- src/editor/graphics/vulkan/vulkan_settings.hpp
+++ src/editor/graphics/vulkan/vulkan_settings.hpp
@@ -14,6 +14,12 @@ inline const std::vector<const char*> DEVICE_EXTENSIONS = {
 };
 
 inline constexpr bool VSYNC{ true };
+
+// We choose the number 2 because we don't want the CPU to get too far ahead of
+// the GPU. With 2 frames in flight, the CPU and the GPU can be working on their
+// own tasks at the same time. If the CPU finishes early, it will wait till the
+// GPU finishes rendering before submitting more work. With 3 or more frames in
+// flight, the CPU could get ahead of the GPU, adding frames of latency.
 inline constexpr int MAX_FRAMES_IN_FLIGHT{ 2 };
 
 inline constexpr struct FaceCullingConfig
@@ -26,16 +32,19 @@ inline const std::vector<const char*> VALIDATION_LAYERS = {
     "VK_LAYER_KHRONOS_validation",
 };
 
-constexpr VkDebugUtilsMessageSeverityFlagsEXT MESSAGE_SEVERITY{
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
-    // VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
-    VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT
-};
+inline constexpr struct DebugMessage
+{
+    VkDebugUtilsMessageSeverityFlagsEXT MESSAGE_SEVERITY{
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+        // VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT
+    };
 
-constexpr VkDebugUtilsMessageTypeFlagsEXT MESSAGE_TYPE{
-    VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
-    VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
-    VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT
-};
+    VkDebugUtilsMessageTypeFlagsEXT MESSAGE_TYPE{
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT
+    };
+} DEBUG_MESSAGE;
 }
