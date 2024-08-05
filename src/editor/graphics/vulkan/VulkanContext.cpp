f4199836e0535ec17182a59fe2299f8f46db7cfe
Spec <dawid.spec.jachowicz@gmail.com>
2024-08-05 08:44:14 +0200

    Add VulkanContext

--- src/editor/graphics/vulkan/VulkanContext.cpp
+++ src/editor/graphics/vulkan/VulkanContext.cpp
@@ -1,5 +1,8 @@
 #include "VulkanContext.hpp"
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 namespace Zeus
 {
 void VulkanContext::glfwKeyCallback(
@@ -9,8 +12,28 @@ void VulkanContext::glfwKeyCallback(
     int action,
     [[maybe_unused]] int mods)
 {
+    auto app =
+        reinterpret_cast<VulkanContext*>(glfwGetWindowUserPointer(window));
+
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+    if (key == GLFW_KEY_C && action == GLFW_PRESS)
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+        app->ref = (app->ref + 1) % 5;
+
+    if (key == GLFW_KEY_A)
+        app->camera.Keyboard('a', 0.1f);
+    if (key == GLFW_KEY_D)
+        app->camera.Keyboard('d', 0.1f);
+
+    if (key == GLFW_KEY_W)
+        app->camera.Keyboard('w', 0.1f);
+
+    if (key == GLFW_KEY_S)
+        app->camera.Keyboard('s', 0.1f);
 }
 
 void VulkanContext::glfwCursorPositionCallback(
@@ -18,26 +41,24 @@ void VulkanContext::glfwCursorPositionCallback(
     [[maybe_unused]] double xpos,
     [[maybe_unused]] double ypos)
 {
-    //   bool firstMouse = true;
-    //   float x = static_cast<float>(xpos);
-    //   float y = static_cast<float>(ypos);
+    auto app =
+        reinterpret_cast<VulkanContext*>(glfwGetWindowUserPointer(window));
 
-    //   if (firstMouse)
-    //   {
-    //       lastX = xpos;
-    //       lastY = ypos;
-    //       firstMouse = false;
-    //   }
+    if (app->firstMouse)
+    {
+        app->lastX = xpos;
+        app->lastY = ypos;
+        app->firstMouse = false;
+    }
 
-    //   float xoffset = xpos - lastX;
-    //   float yoffset =
-    //       lastY - ypos; // reversed since y-coordinates go from bottom to
-    //       top
+    float xoffset = xpos - app->lastX;
+    float yoffset =
+        app->lastY - ypos; // reversed since y-coordinates go from bottom to top
 
-    //   lastX = xpos;
-    //   lastY = ypos;
+    app->lastX = xpos;
+    app->lastY = ypos;
 
-    // trace("Mouse position x:{}, y:{}", xpos, ypos);
+    app->camera.Mouse(xoffset, yoffset);
 }
 
 void VulkanContext::glfwScrollCallback(
@@ -45,6 +66,7 @@ void VulkanContext::glfwScrollCallback(
     [[maybe_unused]] double xoffset,
     [[maybe_unused]] double yoffset)
 {
+    // app->camera.Zoom(yOffset);
     // trace("Mouse scroll x:{}, y:{}", xoffset, yoffset);
 }
 
@@ -53,8 +75,8 @@ void VulkanContext::framebufferResizeCallback(
     [[maybe_unused]] int width,
     [[maybe_unused]] int height)
 {
-    auto app = reinterpret_cast<Zeus::VulkanContext*>(
-        glfwGetWindowUserPointer(window));
+    auto app =
+        reinterpret_cast<VulkanContext*>(glfwGetWindowUserPointer(window));
     app->framebufferResized = true;
 
     // added to make window draw while resizing
