0ced5829bd61abb747ad374aec6794db3427731e
Spec <dawid.spec.jachowicz@gmail.com>
2024-08-08 17:03:17 +0200

    Move vulkan_utils to Engine library

---
+++ src/engine/vulkan/vulkan_utils.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "core/logger.hpp"
+
+#include <vulkan/vulkan.h>
+
+#define CHECK_VKRESULT(expression, msg)                                        \
+    {                                                                          \
+        VkResult checkVkResult = expression;                                   \
+        if (checkVkResult != VK_SUCCESS)                                       \
+        {                                                                      \
+            fatal("{} {}", msg, Zeus::vkResultToString(checkVkResult));        \
+            assert(checkVkResult == VK_SUCCESS);                               \
+        }                                                                      \
+    }
+
+namespace Zeus
+{
+std::uint32_t findMemoryType(
+    const VkPhysicalDevice& physicalDevice,
+    std::uint32_t typeFilter,
+    VkMemoryPropertyFlags properties);
+
+const char* vkResultToString(VkResult result);
+};
