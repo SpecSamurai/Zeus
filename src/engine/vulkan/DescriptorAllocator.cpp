c3eee3eed41fc2850c3dc02d60464fb0d1b9793a
Spec <dawid.spec.jachowicz@gmail.com>
2024-08-27 07:12:23 +0200

    Add DescriptorAllocator

---
+++ src/engine/vulkan/DescriptorAllocator.cpp
@@ -0,0 +1,53 @@
+#include "DescriptorAllocator.hpp"
+
+#include "MemoryAllocator.hpp"
+#include "vulkan_descriptors.hpp"
+
+#include <cstdint>
+#include <vector>
+
+namespace Zeus
+{
+VkDescriptorSet DescriptorAllocator::allocate(
+    VkDevice device,
+    VkDescriptorSetLayout descriptorSetLayout)
+{
+    VkDescriptorSet descriptorSet;
+
+    allocateVkDescriptorSet(
+        descriptorSet,
+        device,
+        descriptorPool,
+        descriptorSetLayout);
+
+    return descriptorSet;
+}
+
+void DescriptorAllocator::initPool(
+    VkDevice device,
+    std::uint32_t maxSets,
+    const std::vector<VkDescriptorPoolSize>& poolSizes,
+    VkDescriptorPoolCreateFlags flags)
+{
+    createDescriptorPool(
+        descriptorPool,
+        device,
+        maxSets,
+        static_cast<std::uint32_t>(poolSizes.size()),
+        poolSizes.data(),
+        flags);
+}
+
+void DescriptorAllocator::clearDescriptors(VkDevice device)
+{
+    vkResetDescriptorPool(device, descriptorPool, 0);
+}
+
+void DescriptorAllocator::destroyPool(VkDevice device)
+{
+    vkDestroyDescriptorPool(
+        device,
+        descriptorPool,
+        MemoryAllocator::pAllocator.get());
+}
+}
---
+++ src/engine/vulkan/DescriptorAllocator.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include <cstdint>
+#include <vector>
+
+namespace Zeus
+{
+class DescriptorAllocator
+{
+public:
+    VkDescriptorSet allocate(
+        VkDevice device,
+        VkDescriptorSetLayout descriptorSetLayout);
+
+    void initPool(
+        VkDevice device,
+        std::uint32_t maxSets,
+        const std::vector<VkDescriptorPoolSize>& poolSizes,
+        VkDescriptorPoolCreateFlags flags = 0);
+
+    void clearDescriptors(VkDevice device);
+    void destroyPool(VkDevice device);
+
+private:
+    VkDescriptorPool descriptorPool{ VK_NULL_HANDLE };
+};
+}
