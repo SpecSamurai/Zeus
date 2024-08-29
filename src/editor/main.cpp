f489add06d0ce3658f1001150972444f75b26be9
Spec <dawid.spec.jachowicz@gmail.com>
2024-08-29 07:01:42 +0200

    Clean-up Editor main function

--- src/editor/main.cpp
+++ src/editor/main.cpp
@@ -6,19 +6,11 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
     Zeus::VulkanContext vkContext;
 
-    vkContext.Init(800, 600, "Zeus");
+    vkContext.init(1440, 1080, "Zeus");
 
-    while (!glfwWindowShouldClose(vkContext.window))
-    {
-        glfwPollEvents();
-        vkContext.DrawFrame();
-        // lastTime = glfwGetTime();
-    }
+    vkContext.run();
 
-    // Wait for operations in a specific command queue to be finished.
-    // Can be used as a rudimentary synchronization
-    vkDeviceWaitIdle(vkContext.device.logicalDevice);
-    vkContext.Destroy();
+    vkContext.destroy();
 
     return EXIT_SUCCESS;
 }
