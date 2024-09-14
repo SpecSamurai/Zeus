74ed5fb3e99d7c9ef0b1424090b2fd34da592cd8
Spec <dawid.spec.jachowicz@gmail.com>
2024-09-14 15:15:36 +0200

    Delete obsolete code

--- prime/src/Editor/App.xaml.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Windows;
-
-namespace Zeus;
-
-public partial class App : Application
-{
-}
--- prime/src/Editor/Serialization/ISerializer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Zeus.Serialization;
-
-public interface ISerializer
-{
-    void ToFile<TInput>(TInput @object, string filePath);
-    TOutput FromFile<TOutput>(string filePath);
-}
