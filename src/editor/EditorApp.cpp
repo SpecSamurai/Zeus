52ffe441c241765e22ea03c4172e9871b58bfd17
Spec <dawid.spec.jachowicz@gmail.com>
2024-10-15 21:16:12 +0200

    Adjusted EditorApp to new entry_point implementation

--- src/editor/EditorApp.cpp
+++ src/editor/EditorApp.cpp
@@ -1,7 +1,14 @@
 #include <application/Application.hpp>
 #include <entry_point.hpp>
 
-Zeus::Application* Zeus::CreateApplication()
+Zeus::Application* Zeus::CreateApplication(CommandLineArgs args)
 {
-    return new Zeus::Application(1440, 1080, "Zeus Editor");
+    ApplicationSpecification specification{
+        .name = "Zeus Editor",
+        .commandLineArgs = args,
+        .windowWidth = 1440,
+        .windowHeight = 1080,
+    };
+
+    return new Zeus::Application(specification);
 }
