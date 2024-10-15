90f173bb361159385f0ff3e0c75836ad66cb79b7
Spec <dawid.spec.jachowicz@gmail.com>
2024-10-15 21:23:32 +0200

    Improved Window class - Removed Vulkan references - Used EventDispatcher - Encapsulated state - Added Getters

--- src/engine/window/Window.cpp
+++ src/engine/window/Window.cpp
@@ -1,76 +1,153 @@
 #include "Window.hpp"
 
+#include "events/KeyEvent.hpp"
+#include "events/MouseEvent.hpp"
+#include "events/WindowEvent.hpp"
+#include "input/MouseButtonCode.hpp"
 #include "logging/logger.hpp"
+#include "math/definitions.hpp"
 #include "window/glfw_utils.hpp"
 
 #define GLFW_INCLUDE_NONE
-#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
 
 #include <cstdint>
 
 namespace Zeus
 {
-Window::Window(std::uint32_t width, std::uint32_t height, const char* title)
-    : title{ title },
-      extent{ .width = width, .height = height }
+Window::Window(const WindowProperties& properties)
+    : m_data{ .title = properties.title,
+              .width = properties.width,
+              .height = properties.height }
 {
 }
 
 void Window::Init()
 {
-    handle = createGlfwWindow(
-        static_cast<int>(extent.width),
-        static_cast<int>(extent.height),
-        title);
+    m_handle = createGlfwWindow(
+        static_cast<int>(m_data.width),
+        static_cast<int>(m_data.height),
+        m_data.title);
 
-    glfwSetWindowUserPointer(handle, this);
-    glfwSetFramebufferSizeCallback(handle, framebufferResizeCallback);
-    glfwSetWindowIconifyCallback(handle, windowIconifyCallback);
+    glfwSetWindowUserPointer(m_handle, &m_data);
+
+    glfwSetWindowSizeCallback(
+        m_handle,
+        [](GLFWwindow* window, int width, int height) {
+            auto windowData{ reinterpret_cast<WindowData*>(
+                glfwGetWindowUserPointer(window)) };
+
+            windowData->width = static_cast<std::uint32_t>(width);
+            windowData->height = static_cast<std::uint32_t>(height);
+
+            // EventDispatcher.Dispatch(WindowResizedEvent{
+            //     .width = windowData->width,
+            //     .height = windowData->height,
+            // });
+        });
+
+    glfwSetWindowCloseCallback(
+        m_handle,
+        []([[maybe_unused]] GLFWwindow* window) {
+            // EventDispatcher.Dispatch(WindowClosedEvent{});
+        });
+
+    glfwSetKeyCallback(
+        m_handle,
+        []([[maybe_unused]] GLFWwindow* window,
+           int key,
+           [[maybe_unused]] int scancode,
+           int action,
+           [[maybe_unused]] int mods) {
+            switch (action)
+            {
+            case GLFW_PRESS: {
+                // EventDispatcher.Dispatch(KeyPressedEvent{
+                //     .keyCode = static_cast<KeyCode>(key),
+                //     .isRepeated = false,
+                // });
+                break;
+            }
+            case GLFW_RELEASE: {
+                // EventDispatcher.Dispatch(KeyReleasedEvent{
+                //     .keyCode = static_cast<KeyCode>(key),
+                // });
+                break;
+            }
+            case GLFW_REPEAT: {
+                // EventDispatcher.Dispatch(KeyPressedEvent{
+                //     .keyCode = static_cast<KeyCode>(key),
+                //     .isRepeated = true,
+                // });
+                break;
+            }
+            }
+        });
+
+    glfwSetCharCallback(
+        m_handle,
+        []([[maybe_unused]] GLFWwindow* window, unsigned int codepoint) {
+            // EventDispatcher.Dispatch(KeyTypedEvent{
+            //     .keyCode = static_cast<KeyCode>(codepoint),
+            // });
+        });
+
+    glfwSetMouseButtonCallback(
+        m_handle,
+        []([[maybe_unused]] GLFWwindow* window,
+           int button,
+           int action,
+           [[maybe_unused]] int mods) {
+            switch (action)
+            {
+            case GLFW_PRESS: {
+                // EventDispatcher.Dispatch(MouseButtonPressedEvent{
+                //     .buttonCode = static_cast<MouseButtonCode>(button),
+                // });
+                break;
+            }
+            case GLFW_RELEASE: {
+                // EventDispatcher.Dispatch(MouseButtonReleasedEvent{
+                //     .buttonCode = static_cast<MouseButtonCode>(button),
+                // });
+                break;
+            }
+            }
+        });
+
+    glfwSetScrollCallback(
+        m_handle,
+        []([[maybe_unused]] GLFWwindow* window,
+           double xOffset,
+           double yOffset) {
+            // EventDispatcher.Dispatch(MouseScrolledEvent{
+            //     .offset = Vector2f(
+            //         static_cast<float>(xOffset),
+            //         static_cast<float>(yOffset)),
+            // });
+        });
+
+    glfwSetCursorPosCallback(
+        m_handle,
+        []([[maybe_unused]] GLFWwindow* window, double xPos, double yPos) {
+            // EventDispatcher.Dispatch(MouseMovedEvent{
+            //     .position = Vector2f(
+            //         static_cast<float>(xPos),
+            //         static_cast<float>(yPos)),
+            // });
+        });
 }
 
 void Window::Destroy()
 {
-    LOG_DEBUG("Destroying window: {}", title);
+    LOG_DEBUG("Destroying window: {}", m_data.title);
 
-    glfwDestroyWindow(handle);
+    glfwDestroyWindow(m_handle);
     glfwTerminate();
 }
 
-void Window::framebufferResizeCallback(
-    GLFWwindow* window,
-    int width,
-    int height)
+void Window::Update()
 {
-    auto obj{ reinterpret_cast<Window*>(glfwGetWindowUserPointer(window)) };
-
-    obj->extent.width = static_cast<std::uint32_t>(width);
-    obj->extent.height = static_cast<std::uint32_t>(height);
-    obj->resized = true;
-    obj->invalidExtent = width == 0 || height == 0;
-}
-
-void Window::windowIconifyCallback(GLFWwindow* window, int iconified)
-{
-    auto obj{ reinterpret_cast<Window*>(glfwGetWindowUserPointer(window)) };
-
-    if (iconified)
-    {
-        obj->extent.width = 0;
-        obj->extent.height = 0;
-        obj->resized = true;
-        obj->invalidExtent = true;
-    }
-    else
-    {
-        int width{ 0 }, height{ 0 };
-        glfwGetFramebufferSize(window, &width, &height);
-
-        obj->extent.width = static_cast<std::uint32_t>(width);
-        obj->extent.height = static_cast<std::uint32_t>(height);
-        obj->resized = true;
-        obj->invalidExtent = width == 0 || height == 0;
-    }
+    glfwPollEvents();
 }
 }
--- src/engine/window/Window.hpp
+++ src/engine/window/Window.hpp
@@ -1,33 +1,56 @@
 #pragma once
 
 #define GLFW_INCLUDE_NONE
-#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
 
 #include <cstdint>
 
 namespace Zeus
 {
+struct WindowProperties
+{
+    const char* title;
+    std::uint32_t width;
+    std::uint32_t height;
+};
+
 class Window
 {
 public:
-    Window(std::uint32_t width, std::uint32_t height, const char* title);
+    Window(const WindowProperties& properties);
     void Init();
     void Destroy();
 
-    GLFWwindow* handle{ nullptr };
-    const char* title;
-    VkExtent2D extent;
-    bool resized{ false };
-    bool invalidExtent{ false };
+    void Update();
+
+    inline constexpr std::uint32_t GetWidth() const
+    {
+        return m_data.width;
+    }
+
+    inline constexpr std::uint32_t GetHeight() const
+    {
+        return m_data.height;
+    }
+
+    inline const char* GetTitle() const
+    {
+        return m_data.title;
+    }
+
+    inline void* GetHandle() const
+    {
+        return m_handle;
+    }
 
 private:
-    static void framebufferResizeCallback(
-        GLFWwindow* window,
-        int width,
-        int height);
+    GLFWwindow* m_handle{ nullptr };
 
-    static void windowIconifyCallback(GLFWwindow* window, int iconified);
+    struct WindowData
+    {
+        const char* title;
+        std::uint32_t width;
+        std::uint32_t height;
+    } m_data;
 };
 }
