4aa0a44defa5c09c5fd5fdcd486c17b1d6d4a196
Spec <dawid.spec.jachowicz@gmail.com>
2024-10-17 00:35:36 +0200

    Added Input manager

---
+++ src/engine/input/Input.cpp
@@ -0,0 +1,65 @@
+#include "Input.hpp"
+
+#include "KeyCode.hpp"
+#include "MouseButtonCode.hpp"
+#include "application/Application.hpp"
+#include "math/definitions.hpp"
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <cstdint>
+
+namespace Zeus
+{
+bool Input::IsKeyDown(const KeyCode keyCode)
+{
+    auto* window{ static_cast<GLFWwindow*>(
+        Application::Instance().GetWindow().GetHandle()) };
+    auto state{ glfwGetKey(window, static_cast<std::int32_t>(keyCode)) };
+
+    return state == GLFW_PRESS;
+}
+
+bool Input::IsKeyUp(const KeyCode keyCode)
+{
+    auto* window{ static_cast<GLFWwindow*>(
+        Application::Instance().GetWindow().GetHandle()) };
+    auto state{ glfwGetKey(window, static_cast<std::int32_t>(keyCode)) };
+
+    return state == GLFW_RELEASE;
+}
+
+bool Input::IsMouseButtonDown(const MouseButtonCode buttonCode)
+{
+    auto* window{ static_cast<GLFWwindow*>(
+        Application::Instance().GetWindow().GetHandle()) };
+    auto state{
+        glfwGetMouseButton(window, static_cast<std::int32_t>(buttonCode))
+    };
+
+    return state == GLFW_PRESS;
+}
+
+bool Input::IsMouseButtonUp(const MouseButtonCode buttonCode)
+{
+    auto* window{ static_cast<GLFWwindow*>(
+        Application::Instance().GetWindow().GetHandle()) };
+    auto state{
+        glfwGetMouseButton(window, static_cast<std::int32_t>(buttonCode))
+    };
+
+    return state == GLFW_RELEASE;
+}
+
+Vector2f Input::GetMousePosition()
+{
+    auto* window{ static_cast<GLFWwindow*>(
+        Application::Instance().GetWindow().GetHandle()) };
+
+    double xPos, yPos;
+    glfwGetCursorPos(window, &xPos, &yPos);
+
+    return Vector2f(static_cast<float>(xPos), static_cast<float>(yPos));
+}
+}
---
+++ src/engine/input/Input.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "KeyCode.hpp"
+#include "MouseButtonCode.hpp"
+#include "math/definitions.hpp"
+
+namespace Zeus
+{
+class Input
+{
+public:
+    static bool IsKeyDown(const KeyCode keyCode);
+    static bool IsKeyUp(const KeyCode keyCode);
+
+    static bool IsMouseButtonDown(const MouseButtonCode buttonCode);
+    static bool IsMouseButtonUp(const MouseButtonCode buttonCode);
+
+    static Vector2f GetMousePosition();
+};
+}
