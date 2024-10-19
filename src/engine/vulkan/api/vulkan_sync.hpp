d25bec5a90519ac962a44976ffe78fc85d90515c
Spec <dawid.spec.jachowicz@gmail.com>
2024-10-19 09:34:07 +0200

    Added API to support Timeline Semaphores

--- src/engine/vulkan/api/vulkan_sync.hpp
+++ src/engine/vulkan/api/vulkan_sync.hpp
@@ -2,8 +2,25 @@
 
 #include <vulkan/vulkan.h>
 
+#include <cstdint>
+
 namespace Zeus
 {
-VkResult createVkSemaphore(const VkDevice& device, VkSemaphore& semaphore);
+VkResult createVkSemaphore(
+    const VkDevice& device,
+    VkSemaphore& semaphore,
+    bool isTimeline = false);
+
 VkResult createVkFence(const VkDevice& device, bool signaled, VkFence& fence);
+
+VkResult waitVkSemaphores(
+    const VkDevice& device,
+    VkSemaphore& semaphore,
+    std::uint64_t value,
+    std::uint64_t timeout_ns);
+
+VkResult signalVkSemaphores(
+    const VkDevice& device,
+    VkSemaphore& semaphore,
+    std::uint64_t value);
 }
