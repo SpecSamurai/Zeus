8e11ccda1d451b63877de2b6747200a176645aee
Spec <dawid.spec.jachowicz@gmail.com>
2024-10-20 17:39:01 +0200

    Added more Resource types

--- src/engine/vulkan/Definitions.hpp
+++ src/engine/vulkan/Definitions.hpp
@@ -1,15 +1,29 @@
 #pragma once
 
+#include <cstdint>
+
 namespace Zeus
 {
-enum class ResourceType
+enum class ResourceType : std::uint8_t
 {
+    Buffer,
+    CommandBuffer,
+    CommandPool,
+    DescriptorSet,
+    DescriptorSetLayout,
     Fence,
+    Image,
+    ImageView,
+    Pipeline,
+    PipelineLayout,
+    Queue,
     Semaphore,
+    Shader,
+    Sampler,
     Count,
 };
 
-enum class QueueType
+enum class QueueType : std::uint8_t
 {
     Graphics,
     Present,
