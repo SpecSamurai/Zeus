17cff28b304aa3c3cd3f4fc5f1630c3ea66df826
Spec <dawid.spec.jachowicz@gmail.com>
2024-10-25 19:39:28 +0200

    Added CommandBuffer and Swapchain classes

---
+++ src/engine/vulkan/CommandBuffer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace Zeus
+{
+class CommandBuffer
+{
+    // void Dispatch(
+    //     uint32_t groupCountX,
+    //     uint32_t groupCountY,
+    //     uint32_t groupCountZ)
+    // {
+    //     assert(m_type == QueueType::Compute);
+    //     // commandBuffer must be in the recording state
+    //
+    //     vkCmdDispatch(m_handle, groupCountX, groupCountY, groupCountZ);
+    // }
+};
+}
---
+++ src/engine/vulkan/Swapchain.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace Zeus
+{
+class Swapchain
+{
+    void Destroy()
+    {
+        // for (auto& imageView : swapchain.imageViews)
+        // {
+        //     vkDestroyImageView(
+        //         device.GetLogicalDevice(),
+        //         imageView,
+        //         allocationCallbacks.get());
+        // }
+        //
+        // vkDestroySwapchainKHR(
+        //     device.GetLogicalDevice(),
+        //     swapchain.handle,
+        //     allocationCallbacks.get());
+    }
+};
+}
