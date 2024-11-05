9c5791626685192cf581f2de9650c15fe727e7a8
Spec <dawid.spec.jachowicz@gmail.com>
2024-11-05 15:25:49 +0100

    Added RasterizationState

---
+++ src/engine/vulkan/RasterizationState.cpp
@@ -0,0 +1,73 @@
+#include "RasterizationState.hpp"
+
+namespace Zeus
+{
+const RasterizationState RasterizationState::Default{ RasterizationState() };
+
+RasterizationState::RasterizationState(
+    VkPolygonMode polygonMode,
+    VkCullModeFlags cullMode,
+    VkFrontFace frontFace,
+    VkBool32 depthClampEnable,
+    VkBool32 depthBiasEnable,
+    float depthBiasConstantFactor,
+    float depthBiasClamp,
+    float depthBiasSlopeFactor,
+    float lineWidth)
+    : m_polygonMode{ polygonMode },
+      m_cullMode{ cullMode },
+      m_frontFace{ frontFace },
+      m_depthBiasConstantFactor{ depthBiasConstantFactor },
+      m_depthBiasClamp{ depthBiasClamp },
+      m_depthBiasSlopeFactor{ depthBiasSlopeFactor },
+      m_lineWidth{ lineWidth },
+      m_depthClampEnable{ depthClampEnable },
+      m_depthBiasEnable{ depthBiasEnable }
+{
+}
+
+VkPolygonMode RasterizationState::GetPolygonMode() const
+{
+    return m_polygonMode;
+}
+
+VkCullModeFlags RasterizationState::GetCullMode() const
+{
+    return m_cullMode;
+}
+
+VkFrontFace RasterizationState::GetFrontFace() const
+{
+    return m_frontFace;
+}
+
+float RasterizationState::GetDepthBiasConstantFactor() const
+{
+    return m_depthBiasConstantFactor;
+}
+
+float RasterizationState::GetDepthBiasClamp() const
+{
+    return m_depthBiasClamp;
+}
+
+float RasterizationState::GetDepthBiasSlopeFactor() const
+{
+    return m_depthBiasSlopeFactor;
+}
+
+float RasterizationState::GetLineWidth() const
+{
+    return m_lineWidth;
+}
+
+VkBool32 RasterizationState::GetDepthClampEnable() const
+{
+    return m_depthClampEnable;
+}
+
+VkBool32 RasterizationState::GetDepthBiasEnable() const
+{
+    return m_depthBiasEnable;
+}
+}
---
+++ src/engine/vulkan/RasterizationState.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <vulkan/vulkan_core.h>
+
+namespace Zeus
+{
+class RasterizationState
+{
+public:
+    RasterizationState(
+        VkPolygonMode polygonMode = VK_POLYGON_MODE_FILL,
+        VkCullModeFlags cullMode = VK_CULL_MODE_NONE,
+        VkFrontFace frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
+        VkBool32 depthClampEnable = VK_FALSE,
+        VkBool32 depthBiasEnable = VK_FALSE,
+        float depthBiasConstantFactor = 0.f,
+        float depthBiasClamp = 0.f,
+        float depthBiasSlopeFactor = 0.f,
+        float lineWidth = 1.f);
+
+    VkPolygonMode GetPolygonMode() const;
+    VkCullModeFlags GetCullMode() const;
+    VkFrontFace GetFrontFace() const;
+
+    float GetDepthBiasConstantFactor() const;
+    float GetDepthBiasClamp() const;
+    float GetDepthBiasSlopeFactor() const;
+    float GetLineWidth() const;
+
+    VkBool32 GetDepthClampEnable() const;
+    VkBool32 GetDepthBiasEnable() const;
+
+    static const RasterizationState Default;
+
+private:
+    VkPolygonMode m_polygonMode;
+    VkCullModeFlags m_cullMode;
+    VkFrontFace m_frontFace;
+
+    float m_depthBiasConstantFactor;
+    float m_depthBiasClamp;
+    float m_depthBiasSlopeFactor;
+    float m_lineWidth;
+
+    VkBool32 m_depthClampEnable;
+    VkBool32 m_depthBiasEnable;
+};
+}
