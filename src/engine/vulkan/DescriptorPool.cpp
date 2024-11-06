565a5381d0c97256c57b64129fcda4b4dcb70ca7
Spec <dawid.spec.jachowicz@gmail.com>
2024-11-06 16:42:30 +0100

    Added DescriptorPool

---
+++ src/engine/vulkan/DescriptorPool.cpp
@@ -0,0 +1,80 @@
+#include "DescriptorPool.hpp"
+
+#include "VkContext.hpp"
+#include "api/vulkan_debug.hpp"
+#include "api/vulkan_descriptors.hpp"
+
+#include <vulkan/vulkan_core.h>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace Zeus
+{
+DescriptorPool::DescriptorPool(const char* name) : m_name(name)
+{
+}
+
+DescriptorPool::~DescriptorPool()
+{
+    if (m_handle == VK_NULL_HANDLE)
+        return;
+
+    VkContext::GetDevice().GetDeletionQueue().Add(
+        ResourceType::DescriptorPool,
+        m_handle);
+    m_handle = VK_NULL_HANDLE;
+}
+
+VkDescriptorSet DescriptorPool::Allocate(
+    VkDescriptorSetLayout descriptorSetLayout)
+{
+    VkDescriptorSet descriptorSet;
+
+    allocateVkDescriptorSet(
+        &descriptorSet,
+        VkContext::GetLogicalDevice(),
+        m_handle,
+        descriptorSetLayout);
+
+    return descriptorSet;
+}
+
+void DescriptorPool::Init(
+    std::uint32_t maxSets,
+    const std::vector<VkDescriptorPoolSize>& poolSizes,
+    VkDescriptorPoolCreateFlags flags)
+{
+    createDescriptorPool(
+        &m_handle,
+        VkContext::GetLogicalDevice(),
+        maxSets,
+        static_cast<std::uint32_t>(poolSizes.size()),
+        poolSizes.data(),
+        flags);
+
+#ifndef NDEBUG
+    if (!m_name.empty())
+    {
+        setDebugUtilsObjectNameEXT(
+            VkContext::GetLogicalDevice(),
+            VK_OBJECT_TYPE_DESCRIPTOR_POOL,
+            reinterpret_cast<std::uint64_t>(m_handle),
+            m_name.c_str());
+    }
+#endif
+}
+
+void DescriptorPool::Reset()
+{
+    VKCHECK(
+        vkResetDescriptorPool(VkContext::GetLogicalDevice(), m_handle, 0),
+        "Failed to reset descriptor pool.");
+}
+
+VkDescriptorPool DescriptorPool::GetHandle() const
+{
+    return m_handle;
+}
+}
---
+++ src/engine/vulkan/DescriptorPool.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vulkan/vulkan_core.h>
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace Zeus
+{
+class DescriptorPool
+{
+public:
+    DescriptorPool(const char* name = "");
+    ~DescriptorPool();
+
+    VkDescriptorSet Allocate(VkDescriptorSetLayout descriptorSetLayout);
+
+    void Init(
+        std::uint32_t maxSets,
+        const std::vector<VkDescriptorPoolSize>& poolSizes,
+        VkDescriptorPoolCreateFlags flags = 0);
+
+    void Reset();
+
+    VkDescriptorPool GetHandle() const;
+
+private:
+    VkDescriptorPool m_handle{ VK_NULL_HANDLE };
+    std::string m_name;
+};
+}
