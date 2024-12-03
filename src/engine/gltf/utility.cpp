d715141bcf30112c0f0e89075a9628ead2c80ece
Spec <dawid.spec.jachowicz@gmail.com>
2024-12-03 23:11:18 +0100

    Added toVkSamplerAddressMode

--- src/engine/gltf/utility.cpp
+++ src/engine/gltf/utility.cpp
@@ -1,7 +1,9 @@
 #include "utility.hpp"
 
-#include <fastgltf/core.hpp>
+#include <fastgltf/types.hpp>
 #include <vulkan/vulkan_core.h>
+
+#include <cassert>
 
 namespace Zeus
 {
@@ -34,6 +36,22 @@ VkSamplerMipmapMode toVkSamplerMipmapMode(fastgltf::Filter filter)
     case fastgltf::Filter::LinearMipMapLinear:
     default:
         return VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    }
+}
+
+VkSamplerAddressMode toVkSamplerAddressMode(fastgltf::Wrap wrap)
+{
+    switch (wrap)
+    {
+    case fastgltf::Wrap::ClampToEdge:
+        return VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+    case fastgltf::Wrap::MirroredRepeat:
+        return VK_SAMPLER_ADDRESS_MODE_MIRRORED_REPEAT;
+    case fastgltf::Wrap::Repeat:
+        return VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    default:
+        assert(false && "Unsupported type");
+        return VK_SAMPLER_ADDRESS_MODE_REPEAT;
     }
 }
 }
--- src/engine/gltf/utility.hpp
+++ src/engine/gltf/utility.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
-#include <fastgltf/core.hpp>
+#include <fastgltf/types.hpp>
 #include <vulkan/vulkan_core.h>
 
 namespace Zeus
 {
 VkFilter toVkFilter(fastgltf::Filter filter);
 VkSamplerMipmapMode toVkSamplerMipmapMode(fastgltf::Filter filter);
+VkSamplerAddressMode toVkSamplerAddressMode(fastgltf::Wrap wrap);
 }
