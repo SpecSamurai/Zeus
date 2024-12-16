8938e702e583a80f0d743142644f4688c54fb488
Spec <dawid.spec.jachowicz@gmail.com>
2024-12-16 21:52:41 +0100

    Added Events support to Application

--- src/engine/application/Application.cpp
+++ src/engine/application/Application.cpp
@@ -1,71 +1,113 @@
 #include "Application.hpp"
 
-#include "core/Timer.hpp"
-#include "input/InputManager.hpp"
+#include "GLFW/glfw3.h"
+#include "core/Engine.hpp"
+#include "events/Event.hpp"
+#include "events/MouseEvent.hpp"
+#include "events/WindowEvent.hpp"
+#include "input/Input.hpp"
+#include "input/KeyCode.hpp"
 #include "logging/logger.hpp"
-#include "rendering/Renderer.hpp"
-#include "vulkan/VulkanContext.hpp"
+#include "math/definitions.hpp"
+#include "profiling/Profiler.hpp"
+#include "rendering/Renderer2.hpp"
+#include "window/Window.hpp"
 
-#include <vulkan/vulkan_core.h>
-
-#include <chrono>
-#include <cstdint>
+#include <cassert>
 #include <thread>
 
 namespace Zeus
 {
-Application::Application(
-    std::uint32_t width,
-    std::uint32_t height,
-    const char* title)
-    : m_window(width, height, title),
-      m_vkContext(),
-      m_renderer(m_window, m_vkContext)
+Application* Application::s_instance{ nullptr };
+Renderer2* renderer2;
+
+Application::Application(const ApplicationSpecification& specification)
+    : m_window(WindowProperties{
+          .title = specification.name,
+          .width = specification.windowWidth,
+          .height = specification.windowHeight,
+      })
 {
+    assert(!s_instance && "Application already created.");
+    s_instance = this;
 }
 
-void Application::Init()
+void Application::Initialize()
 {
-    m_window.Init();
-    m_vkContext.Init(m_window);
-    m_renderer.Init();
+    Event::Dispatcher.Register<WindowClosedEvent>(
+        "Application::WindowClosedEvent",
+        [this](const WindowClosedEvent& event) -> bool {
+            return OnWindowClosed(event);
+        });
 
-    InputManager::Init(m_window.handle);
+    Event::Dispatcher.Register<WindowResizedEvent>(
+        "Application::WindowResizedEvent",
+        [this](const WindowResizedEvent& event) -> bool {
+            return OnWindowResized(event);
+        });
 
+    Event::Dispatcher.Register<MouseMovedEvent>(
+        "Application::MouseMovedEvent",
+        [this](const MouseMovedEvent& event) -> bool {
+            return OnMouseMoved(event);
+        });
+
+    m_window.Initialize();
+
+    Engine::Initialize(m_window);
+    renderer2 = new Renderer2(m_window);
+    renderer2->Initialize();
+    // InputManager::Init((GLFWwindow*)m_window.GetHandle());
     // uiManager.Init(m_window, m_vkContext);
 }
 
 void Application::Run()
 {
-    while (!glfwWindowShouldClose(m_window.handle))
+    m_running = true;
+    // while (!glfwWindowShouldClose((GLFWwindow*)m_window.GetHandle()))
+    while (m_running)
     {
-        Timer timer;
-        timer.Start();
+        Profiler::Begin();
 
-        glfwPollEvents();
+        m_window.Update();
 
-        if (m_window.invalidExtent)
+        if (m_minimized)
         {
-            glfwWaitEvents();
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
             continue;
         }
 
-        if (m_renderer.ResizeRequired())
-        {
-            m_renderer.ResizeDrawObjects(m_window.extent);
-        }
+        // https://gpuopen-librariesandsdks.github.io/VulkanMemoryAllocator/html/staying_within_budget.html
+        // Make sure to call vmaSetCurrentFrameIndex() every frame.
+        // Budget is queried from Vulkan inside of it to avoid overhead of
+        // querying it with every allocation.
+        // vmaSetCurrentFrameIndex(
+        //     vulkan_memory_allocator::allocator,
+        //     static_cast<uint32_t>(frame_count));
+
+        // if (m_renderer.ResizeRequired())
+        // {
+        //     VkExtent2D e{ m_window.GetWidth(), m_window.GetHeight() };
+        //     m_renderer.ResizeDrawObjects(e);
 
         // uiManager.ConfigureFrame();
 
         // imgui commands
         // updateScene();
 
-        m_renderer.Draw();
+        HandleKeyboard();
+        camera->Update();
+        // Engine::GetRenderer().SetCamera(camera->GetViewProjection());
+        // Engine::GetRenderer().Draw();
 
-        timer.End();
-        statistics.frametime = timer.GetMilliseconds();
-        statistics.fps = 1000.f / statistics.frametime;
+        auto a{ camera->GetViewProjection() };
+        renderer2->m_frameDataBuffer.Update(
+            &a,
+            sizeof(camera->GetViewProjection()));
+
+        renderer2->Update();
+
+        Profiler::End();
     }
 }
 
@@ -73,12 +115,98 @@ void Application::Shutdown()
 {
     LOG_DEBUG("Shutting down application");
 
-    vkDeviceWaitIdle(m_vkContext.device.logicalDevice);
+    renderer2->Destroy();
 
-    // uiManager.Destroy(m_vkContext.device.logicalDevice);
-
-    m_renderer.Destroy();
-    m_vkContext.Destroy();
+    Engine::Shutdown();
     m_window.Destroy();
+}
+
+bool Application::OnWindowClosed(
+    [[maybe_unused]] const WindowClosedEvent& event)
+{
+    m_running = false;
+    return true;
+}
+
+bool Application::OnWindowResized(const WindowResizedEvent& event)
+{
+    m_minimized = event.width == 0 || event.height == 0;
+    return true;
+}
+
+void Application::HandleKeyboard()
+{
+    auto speedd = 0.0001f;
+    if (Input::IsKeyDown(KeyCode::E))
+        camera->Move(CameraMovement::UP, speedd);
+
+    if (Input::IsKeyDown(KeyCode::Q))
+        camera->Move(CameraMovement::DOWN, speedd);
+
+    if (Input::IsKeyDown(KeyCode::W))
+        camera->Move(CameraMovement::FORWARD, speedd);
+
+    if (Input::IsKeyDown(KeyCode::S))
+        camera->Move(CameraMovement::BACK, speedd);
+
+    if (Input::IsKeyDown(KeyCode::A))
+        camera->Move(CameraMovement::LEFT, speedd);
+
+    if (Input::IsKeyDown(KeyCode::D))
+        camera->Move(CameraMovement::RIGHT, speedd);
+
+    if (Input::IsKeyDown(KeyCode::C))
+        glfwSetInputMode(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_CURSOR,
+            GLFW_CURSOR_DISABLED);
+
+    if (Input::IsKeyDown(KeyCode::V))
+        glfwSetInputMode(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_CURSOR,
+            GLFW_CURSOR_NORMAL);
+
+    // if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    //     glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+bool Application::OnMouseMoved(const MouseMovedEvent& event)
+{
+    if (camera->IsType(CameraType::FREEFLY) &&
+        glfwGetInputMode(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
+    {
+        return true;
+    }
+
+    if (!glfwGetMouseButton(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_MOUSE_BUTTON_LEFT))
+    {
+        m_isMouseReleased = true;
+        return true;
+    }
+
+    float xPosition{ static_cast<float>(event.position.x) };
+    float yPosition{ static_cast<float>(event.position.y) };
+
+    if (m_isMouseReleased)
+    {
+        m_lastMousePosition.x = xPosition;
+        m_lastMousePosition.y = yPosition;
+        m_isMouseReleased = false;
+    }
+
+    float xOffset{ xPosition - m_lastMousePosition.x };
+    float yOffset{ m_lastMousePosition.y - yPosition };
+
+    m_lastMousePosition.x = xPosition;
+    m_lastMousePosition.y = yPosition;
+
+    camera->OnMouse(xOffset, yOffset);
+
+    return true;
 }
 }
--- src/engine/application/Application.hpp
+++ src/engine/application/Application.hpp
@@ -1,41 +1,68 @@
 #pragma once
 
-// #include "ui/UIManager.hpp"
-
-#include "rendering/Renderer.hpp"
-#include "vulkan/VulkanContext.hpp"
+#include "CommandLineArgs.hpp"
+#include "camera/EditorCamera.hpp"
+#include "camera/camera_utils.hpp"
+#include "events/MouseEvent.hpp"
+#include "events/WindowEvent.hpp"
 #include "window/Window.hpp"
-
-#include <vulkan/vulkan_core.h>
 
 #include <cstdint>
 
 namespace Zeus
 {
-struct EngineStatistics
+struct ApplicationSpecification
 {
-    float frametime;
-    float fps;
-    float meshDrawTime;
-    float sceneUpdateTime;
-    int triangleCount;
-    int drawcallCount;
+    const char* name;
+    CommandLineArgs commandLineArgs;
+    std::uint32_t windowWidth;
+    std::uint32_t windowHeight;
 };
 
 class Application
 {
 public:
-    Application(std::uint32_t width, std::uint32_t height, const char* title);
-    void Init();
+    Application(const ApplicationSpecification& specification);
+    void Initialize();
     void Run();
     void Shutdown();
 
-private:
-    Zeus::Window m_window;
-    Zeus::VulkanContext m_vkContext;
-    Zeus::Renderer m_renderer;
+    inline const Window& GetWindow() const
+    {
+        return m_window;
+    }
 
-    EngineStatistics statistics;
-    // UIManager uiManager;
+    inline static const Application& Instance()
+    {
+        return *s_instance;
+    }
+
+private:
+    bool OnWindowClosed(const WindowClosedEvent& event);
+    bool OnWindowResized(const WindowResizedEvent& event);
+
+    void HandleKeyboard();
+
+    bool OnMouseMoved(const MouseMovedEvent& event);
+
+private:
+    static Application* s_instance;
+
+    inline static std::unique_ptr<EditorCamera> camera =
+        createFreeflyEditorCamera(1440.f / 1080.f);
+
+    inline static struct MousePosition
+    {
+        float x;
+        float y;
+    } m_lastMousePosition;
+    inline static bool m_isMouseReleased{ true };
+
+    Window m_window;
+
+    bool m_running{ false };
+    bool m_minimized{ false };
 };
+
+[[nodiscard]] extern Application* CreateApplication(CommandLineArgs args);
 }
