47b9defbc7cc19ab2493fa13e306ce664d81550c
Spec <dawid.spec.jachowicz@gmail.com>
2025-02-10 22:42:49 +0100

    Extended ComponentSparseSetTest

--- tests/engine/ecs/ComponentSparseSetTest.cpp
+++ tests/engine/ecs/ComponentSparseSetTest.cpp
@@ -7,7 +7,7 @@ struct TestComponent
     float x{}, y{};
 };
 
-TEST(ComponentSparseSetTest, Push_EntityWithDefaultComponent)
+TEST(ComponentSparseSetTest, Push_Entity_EntityHasEmptyComponent)
 {
     Zeus::ECS::ComponentSparseSet<TestComponent> sut;
     Zeus::ECS::Entity entity{ 0 };
@@ -76,23 +76,30 @@ TEST(ComponentSparseSetTest, Emplace_RValueComponent)
 TEST(ComponentSparseSetTest, Emplace_RValueArguments)
 {
     Zeus::ECS::ComponentSparseSet<TestComponent> sut;
-    Zeus::ECS::Entity entity{ 0 };
+    Zeus::ECS::Entity entity1{ 0 };
+    Zeus::ECS::Entity entity2{ 2 };
 
-    sut.Emplace(entity, 1, 2);
+    sut.Emplace(entity1, 1, 2);
+    sut.Emplace(entity2, 3, 4);
 
     auto empty = sut.Empty();
     auto capacity = sut.Capacity();
     auto size = sut.Size();
-    auto contains = sut.Contains(entity);
-    auto& actual = sut.Get(entity);
+    auto contains1 = sut.Contains(entity1);
+    auto contains2 = sut.Contains(entity2);
+    auto& actual1 = sut.Get(entity1);
+    auto& actual2 = sut.Get(entity2);
 
     EXPECT_FALSE(empty);
-    EXPECT_EQ(capacity, 1);
-    EXPECT_EQ(size, 1);
-    EXPECT_TRUE(contains);
+    EXPECT_EQ(capacity, 2);
+    EXPECT_EQ(size, 2);
+    EXPECT_TRUE(contains1);
+    EXPECT_TRUE(contains2);
 
-    EXPECT_EQ(actual.x, 1);
-    EXPECT_EQ(actual.y, 2);
+    EXPECT_EQ(actual1.x, 1);
+    EXPECT_EQ(actual1.y, 2);
+    EXPECT_EQ(actual2.x, 3);
+    EXPECT_EQ(actual2.y, 4);
 }
 
 TEST(ComponentSparseSetTest, Emplace_LValueComponent)
@@ -113,6 +120,61 @@ TEST(ComponentSparseSetTest, Emplace_LValueComponent)
     EXPECT_EQ(capacity, 1);
     EXPECT_EQ(size, 1);
     EXPECT_TRUE(contains);
+
+    EXPECT_EQ(actual.x, 1);
+    EXPECT_EQ(actual.y, 2);
+}
+
+TEST(ComponentSparseSetTest, Patch_ModifyComponent)
+{
+    Zeus::ECS::ComponentSparseSet<TestComponent> sut;
+    TestComponent component{ .x = 1, .y = 2 };
+    Zeus::ECS::Entity entity{ 0 };
+    sut.Emplace(entity, component);
+
+    sut.Patch(entity, [](TestComponent& value) {
+        value.x = 42;
+        value.y = 24;
+    });
+
+    auto empty = sut.Empty();
+    auto capacity = sut.Capacity();
+    auto size = sut.Size();
+    auto contains = sut.Contains(entity);
+    auto& actual = sut.Get(entity);
+
+    EXPECT_FALSE(empty);
+    EXPECT_EQ(capacity, 1);
+    EXPECT_EQ(size, 1);
+    EXPECT_TRUE(contains);
+
+    EXPECT_EQ(actual.x, 42);
+    EXPECT_EQ(actual.y, 24);
+}
+
+TEST(ComponentSparseSetTest, Erase_EntityNotFound)
+{
+    Zeus::ECS::ComponentSparseSet<TestComponent> sut;
+    TestComponent component{ .x = 1, .y = 2 };
+    Zeus::ECS::Entity entity1{ 0 };
+    Zeus::ECS::Entity entity2{ 1 };
+    sut.Emplace(entity1, component);
+    sut.Emplace(entity2, component);
+
+    sut.Erase(entity1);
+
+    auto empty = sut.Empty();
+    auto capacity = sut.Capacity();
+    auto size = sut.Size();
+    auto contains1 = sut.Contains(entity1);
+    auto contains2 = sut.Contains(entity2);
+    auto& actual = sut.Get(entity2);
+
+    EXPECT_FALSE(empty);
+    EXPECT_EQ(capacity, 2);
+    EXPECT_EQ(size, 1);
+    EXPECT_FALSE(contains1);
+    EXPECT_TRUE(contains2);
 
     EXPECT_EQ(actual.x, 1);
     EXPECT_EQ(actual.y, 2);
