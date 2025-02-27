8a7f362b7bdde7f2e46775d8f44928b51366f99f
Spec <dawid.spec.jachowicz@gmail.com>
2025-02-27 23:19:23 +0100

    Implemented EditorApp

--- src/editor/EditorApp.cpp
+++ src/editor/EditorApp.cpp
@@ -1,7 +1,20 @@
+#include "EditorApp.hpp"
+
 #include <application/Application.hpp>
 #include <application/entry_point.hpp>
+#include <assets/AssetsManager.hpp>
+#include <core/Engine.hpp>
+#include <events/Event.hpp>
+#include <events/MouseEvent.hpp>
+#include <events/WindowEvent.hpp>
+#include <input/Input.hpp>
+#include <input/KeyCode.hpp>
+#include <logging/logger.hpp>
+#include <profiling/Profiler.hpp>
 
-Zeus::Application* Zeus::CreateApplication(CommandLineArgs args)
+namespace Zeus
+{
+Application* CreateApplication(CommandLineArgs args)
 {
     ApplicationSpecification specification{
         .name = "Zeus Editor",
@@ -10,5 +23,197 @@ Zeus::Application* Zeus::CreateApplication(CommandLineArgs args)
         .windowHeight = 1080,
     };
 
-    return new Zeus::Application(specification);
+    return new EditorApp(specification);
+}
+
+EditorApp::EditorApp(const ApplicationSpecification& specification)
+    : Application(specification)
+{
+}
+
+void EditorApp::Initialize()
+{
+    LOG_DEBUG("Initializing Editor Application");
+
+    Engine::Initialize(m_window);
+
+    Event::Dispatcher.Register<WindowClosedEvent>(
+        "Application::WindowClosedEvent",
+        [this](const WindowClosedEvent& event) -> bool {
+            return OnWindowClosed(event);
+        });
+
+    Event::Dispatcher.Register<WindowResizedEvent>(
+        "Application::WindowResizedEvent",
+        [this](const WindowResizedEvent& event) -> bool {
+            return OnWindowResized(event);
+        });
+
+    Event::Dispatcher.Register<MouseMovedEvent>(
+        "Application::MouseMovedEvent",
+        [this](const MouseMovedEvent& event) -> bool {
+            return OnMouseMoved(event);
+        });
+
+    Engine::Renderer().DrawTriangle(
+        Math::Vector3f(0.0f, -0.5f, 0.0f),
+        Math::Vector3f(0.5, 0.5, 0),
+        Math::Vector3f(-0.5, 0.5, 0),
+        Math::Colors::GREEN);
+
+    Engine::Renderer().DrawRectangle(
+        Math::Vector3f(0.0f, 0.0f, 0.0f),
+        Math::Vector3f(0.5, 0.0, 0),
+        Math::Vector3f(0.5, 0.5, 0),
+        Math::Vector3f(0, 0.5, 0),
+        Math::Colors::BLUE);
+
+    Engine::Renderer().DrawLine(
+        Math::Vector3f(0.0f, 0.0f, 0.0f),
+        Math::Vector3f(0.5f, -0.3f, 0.3f),
+        Math::Colors::MAGENTA,
+        Math::Colors::CYAN);
+
+    Engine::Renderer().m_linesVertexBuffer.Update(
+        Engine::Renderer().m_lines.data(),
+        Engine::Renderer().m_lines.size() * sizeof(Vertex_PositionColor));
+
+    auto asset =
+        AssetsManager::GetObjLoader()->Load("../../models/viking_room.obj");
+
+    Engine::Renderer().m_meshVertexBuffer.Update(
+        asset->mesh->m_vertices.data(),
+        asset->mesh->m_vertices.size() * sizeof(Vertex));
+}
+
+void EditorApp::Run()
+{
+    m_running = true;
+
+    while (m_running)
+    {
+        Profiler::Begin();
+
+        m_window.Update();
+
+        if (m_minimized)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        // https://gpuopen-librariesandsdks.github.io/VulkanMemoryAllocator/html/staying_within_budget.html
+        // Make sure to call vmaSetCurrentFrameIndex() every frame.
+        // Budget is queried from Vulkan inside of it to avoid overhead of
+        // querying it with every allocation.
+        // vmaSetCurrentFrameIndex(
+        //     vulkan_memory_allocator::allocator,
+        //     static_cast<uint32_t>(frame_count));
+
+        HandleKeyboard();
+        camera->Update();
+
+        /*Engine::GetRenderer().SetCamera(camera->GetViewProjection());*/
+        Engine::Renderer().SetCameraProjection(camera->GetViewProjection());
+        Engine::Update();
+
+        Profiler::End();
+    }
+}
+
+void EditorApp::Shutdown()
+{
+    LOG_DEBUG("Shutting down Editor Application");
+
+    Engine::Shutdown();
+    m_window.Destroy();
+}
+
+bool EditorApp::OnWindowClosed([[maybe_unused]] const WindowClosedEvent& event)
+{
+    m_running = false;
+    return true;
+}
+
+bool EditorApp::OnWindowResized(const WindowResizedEvent& event)
+{
+    m_minimized = event.width == 0 || event.height == 0;
+    return true;
+}
+
+void EditorApp::HandleKeyboard()
+{
+    auto speedd = 0.0001f;
+    if (Input::IsKeyDown(KeyCode::E))
+        camera->Move(CameraMovement::UP, speedd);
+
+    if (Input::IsKeyDown(KeyCode::Q))
+        camera->Move(CameraMovement::DOWN, speedd);
+
+    if (Input::IsKeyDown(KeyCode::W))
+        camera->Move(CameraMovement::FORWARD, speedd);
+
+    if (Input::IsKeyDown(KeyCode::S))
+        camera->Move(CameraMovement::BACK, speedd);
+
+    if (Input::IsKeyDown(KeyCode::A))
+        camera->Move(CameraMovement::LEFT, speedd);
+
+    if (Input::IsKeyDown(KeyCode::D))
+        camera->Move(CameraMovement::RIGHT, speedd);
+
+    if (Input::IsKeyDown(KeyCode::C))
+        glfwSetInputMode(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_CURSOR,
+            GLFW_CURSOR_DISABLED);
+
+    if (Input::IsKeyDown(KeyCode::V))
+        glfwSetInputMode(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_CURSOR,
+            GLFW_CURSOR_NORMAL);
+
+    // if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    //     glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+bool EditorApp::OnMouseMoved(const MouseMovedEvent& event)
+{
+    if (camera->IsType(CameraType::FREEFLY) &&
+        glfwGetInputMode(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
+    {
+        return true;
+    }
+
+    if (!glfwGetMouseButton(
+            reinterpret_cast<GLFWwindow*>(m_window.GetHandle()),
+            GLFW_MOUSE_BUTTON_LEFT))
+    {
+        m_isMouseReleased = true;
+        return true;
+    }
+
+    float xPosition{ static_cast<float>(event.position.x) };
+    float yPosition{ static_cast<float>(event.position.y) };
+
+    if (m_isMouseReleased)
+    {
+        m_lastMousePosition.x = xPosition;
+        m_lastMousePosition.y = yPosition;
+        m_isMouseReleased = false;
+    }
+
+    float xOffset{ xPosition - m_lastMousePosition.x };
+    float yOffset{ m_lastMousePosition.y - yPosition };
+
+    m_lastMousePosition.x = xPosition;
+    m_lastMousePosition.y = yPosition;
+
+    camera->OnMouse(xOffset, yOffset);
+
+    return true;
+}
 }
---
+++ src/editor/EditorApp.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "camera/EditorCamera.hpp"
+#include "camera/camera_utils.hpp"
+#include <application/Application.hpp>
+#include <events/MouseEvent.hpp>
+#include <events/WindowEvent.hpp>
+#include <window/Window.hpp>
+
+namespace Zeus
+{
+class EditorApp : public Application
+{
+public:
+    EditorApp(const ApplicationSpecification& specification);
+
+    void Initialize() override;
+    void Run() override;
+    void Shutdown() override;
+
+private:
+    bool OnWindowClosed(const WindowClosedEvent& event);
+    bool OnWindowResized(const WindowResizedEvent& event);
+    bool OnMouseMoved(const MouseMovedEvent& event);
+
+    void HandleKeyboard();
+
+private:
+    inline static std::unique_ptr<EditorCamera> camera =
+        createFreeflyEditorCamera(1440.f / 1080.f);
+
+    inline static struct MousePosition
+    {
+        float x;
+        float y;
+    } m_lastMousePosition;
+    inline static bool m_isMouseReleased{ true };
+
+    bool m_running{ false };
+    bool m_minimized{ false };
+};
+}
