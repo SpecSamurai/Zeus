c3bcf1c36e6ef0e026b418ca51396a17d52dc1f8
Spec <dawid.spec.jachowicz@gmail.com>
2025-04-01 13:33:10 +0200

    Added example class for WorldViewer widget

---
+++ src/editor/widgets/WorldViewer.cpp
@@ -0,0 +1,14 @@
+#include "WorldViewer.hpp"
+
+#include <imgui.h>
+
+namespace Zeus
+{
+void WorldViewer::Update()
+{
+    if (ImGui::Begin("World Viewer"))
+    {
+    }
+    ImGui::End();
+}
+}
---
+++ src/editor/widgets/WorldViewer.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Widget.hpp"
+
+namespace Zeus
+{
+class WorldViewer : public Widget
+{
+    void Update() override;
+};
+}
