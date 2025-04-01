79d1f65d8a5194a42b84f0b98dc777065b8ba623
Spec <dawid.spec.jachowicz@gmail.com>
2025-04-01 13:32:45 +0200

    Added example class for Settings widget

---
+++ src/editor/widgets/Settings.cpp
@@ -0,0 +1,14 @@
+#include "Settings.hpp"
+
+#include <imgui.h>
+
+namespace Zeus
+{
+void Settings::Update()
+{
+    if (ImGui::Begin("Settings"))
+    {
+    }
+    ImGui::End();
+}
+}
---
+++ src/editor/widgets/Settings.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Widget.hpp"
+
+namespace Zeus
+{
+class Settings : public Widget
+{
+    void Update() override;
+};
+}
