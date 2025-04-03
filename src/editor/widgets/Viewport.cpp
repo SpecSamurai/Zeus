0e4b9d5367729d1378f5cf08c827eab3fd6f45d2
Spec <dawid.spec.jachowicz@gmail.com>
2025-04-03 23:38:47 +0200

    Added Statistics and Toolbar to Viewport

--- src/editor/widgets/Viewport.cpp
+++ src/editor/widgets/Viewport.cpp
@@ -1,11 +1,20 @@
 #include "Viewport.hpp"
 
+#include "UserInterface.hpp"
+#include "events/Event.hpp"
+#include "input/KeyCode.hpp"
+
 #include <backends/imgui_impl_vulkan.h>
 #include <core/Engine.hpp>
 #include <imgui.h>
+#include <profiling/Profiler.hpp>
 
 namespace Zeus
 {
+Viewport::Viewport(UserInterface* root) : m_root{ root }
+{
+}
+
 void Viewport::Initialize()
 {
     m_image =
@@ -36,7 +45,51 @@ void Viewport::Update()
             ImVec2(1, 1),
             ImVec4(1, 1, 1, 1),
             ImColor(0, 0, 0, 0));
+
+        ShowToolbar(m_root->options.toolbar);
+        ShowStatistics(m_root->options.statistics);
     }
     ImGui::End();
+}
+
+void Viewport::ShowToolbar(bool show)
+{
+    if (show)
+    {
+        ImVec2 parentSize{ ImGui::GetWindowSize() };
+
+        ImGui::SetCursorPos(ImVec2(parentSize.x - 200, 30));
+        ImGui::SetNextWindowBgAlpha(.9f);
+
+        if (ImGui::BeginChild("Statistics", ImVec2(175, 102), true))
+        {
+            ImGui::Text("Frametime: %.4f ms", Profiler::LastFrametime());
+            ImGui::Text("FPS: %.1f", Profiler::FPS());
+            ImGui::Text("Draw time: %.4f ms", Profiler::DrawTime());
+            ImGui::Text("Update time: %.4f ms", Profiler::UpdateTime());
+            ImGui::Text("Triangles: %i", Profiler::TriangleCount());
+            ImGui::Text("Draws: %i", Profiler::DrawCallCount());
+        }
+        ImGui::EndChild();
+    }
+}
+
+void Viewport::ShowStatistics(bool show)
+{
+    if (show)
+    {
+        ImGui::SetCursorPos(ImVec2(10, 30));
+        ImGui::SetNextWindowBgAlpha(.9f);
+
+        if (ImGui::BeginChild("Toolbar", ImVec2(19, 23), true))
+        {
+            if (ImGui::Button("X"))
+            {
+                Zeus::Event::Dispatcher.Dispatch(
+                    KeyPressedEvent{ .keyCode = KeyCode::C });
+            }
+        }
+        ImGui::EndChild();
+    }
 }
 }
--- src/editor/widgets/Viewport.hpp
+++ src/editor/widgets/Viewport.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "UserInterface.hpp"
 #include "Widget.hpp"
 
 #include <rhi/Image.hpp>
@@ -10,10 +11,17 @@ namespace Zeus
 class Viewport : public Widget
 {
 public:
+    Viewport(UserInterface* root);
+
     void Initialize() override;
     void Update() override;
 
 private:
+    void ShowToolbar(bool show);
+    void ShowStatistics(bool show);
+
+private:
+    UserInterface* m_root;
     const Image* m_image{ nullptr };
     VkDescriptorSet m_descriptorSet{ VK_NULL_HANDLE };
 };
