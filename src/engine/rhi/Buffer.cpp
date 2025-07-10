9d71376de405cd1b6534539817783ba05c4ad9fe
Spec <dawid.spec.jachowicz@gmail.com>
2025-07-10 19:09:52 +0200

    Buffer supports arrays of items

--- src/engine/rhi/Buffer.cpp
+++ src/engine/rhi/Buffer.cpp
@@ -1,6 +1,7 @@
 #include "Buffer.hpp"
 
 #include "VkContext.hpp"
+#include "memory/memory.hpp"
 #include "vulkan/vulkan_debug.hpp"
 
 #include <vulkan/vulkan_core.h>
@@ -14,18 +15,40 @@ namespace Zeus
 Buffer::Buffer(
     std::string_view name,
     VkBufferUsageFlags usage,
-    VkDeviceSize size,
+    VkDeviceSize itemSize,
+    std::uint64_t count,
     VkMemoryPropertyFlags memoryPropertyFlags,
     bool mapped)
-    : m_usage{ usage },
+    : m_count{ count },
+      m_usage{ usage },
       m_name{ name },
       m_mapped{ mapped }
 {
+    assert(itemSize > 0 && "Invalid itemSize");
+    assert(count > 0 && "Invalid count");
+
+    if (m_usage & VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT)
+    {
+        m_stride = alignUp(
+            itemSize,
+            VkContext::Device().GetLimits().minUniformBufferOffsetAlignment);
+    }
+    else if (m_usage & VK_BUFFER_USAGE_STORAGE_BUFFER_BIT)
+    {
+        m_stride = alignUp(
+            itemSize,
+            VkContext::Device().GetLimits().minStorageBufferOffsetAlignment);
+    }
+    else
+    {
+        m_stride = itemSize;
+    }
+
     VkBufferCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
     createInfo.pNext = nullptr;
     createInfo.flags = 0;
-    createInfo.size = size;
+    createInfo.size = m_stride * m_count;
     createInfo.usage = usage;
     createInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
     createInfo.queueFamilyIndexCount = 0;
@@ -83,11 +106,23 @@ Buffer::Buffer(
     VkContext::SetDebugName(VK_OBJECT_TYPE_BUFFER, m_handle, m_name);
 }
 
+Buffer::Buffer(
+    std::string_view name,
+    VkBufferUsageFlags usage,
+    VkDeviceSize size,
+    VkMemoryPropertyFlags memoryPropertyFlags,
+    bool mapped)
+    : Buffer(name, usage, size, 1, memoryPropertyFlags, mapped)
+{
+}
+
 Buffer::Buffer(Buffer&& other) noexcept
     : m_handle{ other.m_handle },
       m_allocation{ other.m_allocation },
       m_info{ other.m_info },
       m_deviceAddress{ other.m_deviceAddress },
+      m_stride{ other.m_stride },
+      m_count{ other.m_count },
       m_usage{ other.m_usage },
       m_name{ other.m_name },
       m_mapped{ other.m_mapped }
@@ -111,9 +146,11 @@ Buffer& Buffer::operator=(Buffer&& other)
         m_allocation = other.m_allocation;
         m_info = other.m_info;
         m_deviceAddress = other.m_deviceAddress;
+        m_stride = other.m_stride;
+        m_count = other.m_count;
         m_usage = other.m_usage;
-        m_mapped = other.m_mapped;
         m_name = other.m_name;
+        m_mapped = other.m_mapped;
 
         other.m_handle = VK_NULL_HANDLE;
         other.m_allocation = VK_NULL_HANDLE;
@@ -169,16 +206,34 @@ void Buffer::Update(const void* data, std::size_t size, std::size_t srcOffset)
 
         VkContext::Device().CmdImmediateSubmit(
             [&](const CommandBuffer& commandBuffer) {
+                VkBufferCopy2 regions[1]{
+                    VkBufferCopy2{
+                        .sType = VK_STRUCTURE_TYPE_BUFFER_COPY_2,
+                        .pNext = nullptr,
+                        .srcOffset = 0,
+                        .dstOffset = srcOffset,
+                        .size = staging.GetSize(),
+                    },
+                };
+
                 commandBuffer.CopyBuffer(
                     staging.GetHandle(),
                     m_handle,
-                    staging.GetSize(),
-                    0,
-                    srcOffset);
+                    regions);
             });
 
         staging.Destroy();
     }
+}
+
+void Buffer::UpdateAt(const void* data, std::size_t size, std::uint64_t index)
+{
+    assert(size <= m_stride && "Invalid size");
+    assert(index < m_count && "Invalid index");
+
+    std::size_t srcOffset{ index * m_stride };
+
+    Update(data, size, srcOffset);
 }
 
 void* Buffer::GetData() const
@@ -211,6 +266,16 @@ VkDeviceAddress Buffer::GetDeviceAddress() const
 {
     assert(m_usage & VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT);
     return m_deviceAddress;
+}
+
+VkDeviceSize Buffer::GetStride() const
+{
+    return m_stride;
+}
+
+std::uint64_t Buffer::GetCount() const
+{
+    return m_count;
 }
 
 VkBufferUsageFlags Buffer::GetUsage() const
--- src/engine/rhi/Buffer.hpp
+++ src/engine/rhi/Buffer.hpp
@@ -6,6 +6,7 @@
 
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <string_view>
 
 namespace Zeus
@@ -14,6 +15,15 @@ class Buffer
 {
 public:
     Buffer() = default;
+
+    Buffer(
+        std::string_view name,
+        VkBufferUsageFlags usage,
+        VkDeviceSize itemSize,
+        std::uint64_t count,
+        VkMemoryPropertyFlags memoryPropertyFlags,
+        bool mapped = false);
+
     Buffer(
         std::string_view name,
         VkBufferUsageFlags usage,
@@ -32,14 +42,21 @@ public:
     void Destroy();
 
     void Update(const void* data, std::size_t size, std::size_t srcOffset = 0);
+    void UpdateAt(const void* data, std::size_t size, std::uint64_t index);
 
     void* GetData() const;
 
     template <typename T>
-    inline T* GetData() const
+    inline T* GetData(std::uint64_t index = 0) const
     {
         assert(m_mapped);
-        return reinterpret_cast<T*>(m_info.pMappedData);
+        assert(index < m_count && "Invalid index");
+        assert(sizeof(T) <= m_stride);
+
+        auto offset{ reinterpret_cast<std::byte*>(m_info.pMappedData) +
+                     (index * m_stride) };
+
+        return reinterpret_cast<T*>(offset);
     }
 
     VkBuffer GetHandle() const;
@@ -47,7 +64,8 @@ public:
     const VmaAllocationInfo& GetInfo() const;
     VkDeviceSize GetSize() const;
     VkDeviceAddress GetDeviceAddress() const;
-
+    VkDeviceSize GetStride() const;
+    std::uint64_t GetCount() const;
     VkBufferUsageFlags GetUsage() const;
 
     inline constexpr std::string_view GetName() const
@@ -60,11 +78,10 @@ private:
     VmaAllocation m_allocation{ VK_NULL_HANDLE };
     VmaAllocationInfo m_info{};
     VkDeviceAddress m_deviceAddress{};
-
+    VkDeviceSize m_stride{};
+    std::uint64_t m_count{};
     VkBufferUsageFlags m_usage{};
-
     std::string_view m_name;
-
-    bool m_mapped;
+    bool m_mapped{};
 };
 }
